4d4a4f1a0107f2081935a58cd3b21fa9d49865cd
Sam Kozin <i.a.m@bk.ru>
2015-09-27 22:27:12 +0300

    Disable Redux logging in production

--- src/Redux/configure-store.js
+++ src/Redux/configure-store.js
@@ -3,11 +3,15 @@ import loggerMiddleware from 'redux-logger'
 import {apiMiddleware} from './middlewares'
 import * as reducers from './reducers'
 
+let middleware = [ apiMiddleware ]
+
+if (process.env.NODE_ENV != 'production') {
+  middleware.push(loggerMiddleware())
+}
+
+const storeEnhancer = applyMiddleware.apply(null, middleware)
+const createStoreWithMiddleware = storeEnhancer(createStore)
 const reducer = combineReducers(reducers)
-const createStoreWithMiddleware = applyMiddleware(
-  apiMiddleware,
-  loggerMiddleware()
-)(createStore)
 
 export default function configureStore(initialState) {
   return createStoreWithMiddleware(reducer, initialState)
