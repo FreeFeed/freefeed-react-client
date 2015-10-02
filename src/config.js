52bdea8cc3eda2497beb4e4786f5c1c5887b227b
Berkus Decker <berkus@madfire.net>
2015-10-02 10:41:16 +0300

    Add sentinel block.

--- src/config.js
+++ src/config.js
@@ -1,14 +1,17 @@
 const config = {
   api:{
-    host: 'http://localhost:3000'
+    host: 'http://localhost:3000',
+    sentinel: null // keep always last
   },
   auth: {
     cookieDomain: 'localhost',
     tokenPrefix: 'freefeed_',
     userStorageKey: 'USER_KEY',
+    sentinel: null // keep always last
   },
   user:{
-    userNameMode: 'user'
+    userNameMode: 'user',
+    sentinel: null // keep always last
   },
 }
 
