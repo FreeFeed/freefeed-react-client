e67afc1691d90c79798dd6d33b5b93791f16db72
Alex Olshansky <i@creagenics.com>
2015-10-31 18:39:57 +0100

    [attachments] Add tooltip with file name and formatted file size

--- src/components/post-attachment.jsx
+++ src/components/post-attachment.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import numeral from 'numeral'
 
 export default (props) => {
+  const formattedFileSize = numeral(props.fileSize).format('0.[0] b')
+  const nameAndSize = props.fileName + ' (' + formattedFileSize + ')'
+
   return (
     <div className='attachment'>
-      <a href={props.url} target="_blank">
+      <a href={props.url} title={nameAndSize} target="_blank">
         {props.thumbnailUrl ? (
-          <img src={props.thumbnailUrl} />
+          <img src={props.thumbnailUrl} alt={nameAndSize} />
         ) : (
           props.id
         )}
