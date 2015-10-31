897562b48806407ac9fa4c325f09e21c0d2735f2
Alex Olshansky <i@creagenics.com>
2015-10-31 22:14:29 +0100

    [attachments] Split post-attachment into two components

--- src/components/post-attachments.jsx
+++ src/components/post-attachments.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
-import PostAttachment from './post-attachment'
+import ImageAttachment from './post-attachment-image'
+import GeneralAttachment from './post-attachment-general'
 
 export default (props) => {
   const attachments = props.attachments || []
 
   const imageAttachments = attachments
     .filter(attachment => attachment.mediaType === 'image')
-    .map(attachment => (<PostAttachment key={attachment.id} {...attachment}/>))
+    .map(attachment => (<ImageAttachment key={attachment.id} {...attachment}/>))
 
   const generalAttachments = attachments
     .filter(attachment => attachment.mediaType === 'general')
-    .map(attachment => (<PostAttachment key={attachment.id} {...attachment}/>))
+    .map(attachment => (<GeneralAttachment key={attachment.id} {...attachment}/>))
 
   return (
     <div className='attachments'>
