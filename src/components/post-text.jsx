e08b4b39659afba2551053f54518a436025cc049
Victor Suzdalev <victor.suzdalev@gmail.com>
2015-11-11 22:42:20 +0300

    Making react happy with post text

--- src/components/post-text.jsx
+++ src/components/post-text.jsx
@@ -5,11 +5,11 @@ const allButLast = arr => arr.slice(0, arr.length-1)
 
 const brAndTrim = (text) => {
   const lines = text.split(/\n/g).map(line => line.trim()).filter(line=>line)
-  const linesWithBrs = allButLast(lines).map(line => <span>{line}<br/></span>)
+  const linesWithBrs = allButLast(lines).map((line, i) => <span key={i}>{line}<br/></span>)
   return [...linesWithBrs, lines[lines.length-1]]
 }
 
-const p = (text) => <p>{text}</p>
+const p = (text, i) => <p key={i}>{text}</p>
 
 const getEntered = text => {
   if (!/\n/.test(text)){
