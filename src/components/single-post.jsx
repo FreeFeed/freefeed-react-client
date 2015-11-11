5e52258156b5c1ae253718798e587fd610aaa234
Victor Suzdalev <victor.suzdalev@gmail.com>
2015-11-11 22:43:29 +0300

    Fixing single-post route blinking

--- src/components/single-post.jsx
+++ src/components/single-post.jsx
@@ -51,7 +51,7 @@ function selectState(state) {
   const boxHeader = state.boxHeader
   const user = state.user
 
-  const post = joinPostData(state)(state.feedViewState.feed[0])
+  const post = joinPostData(state)(state.singlePostId)
   const viewState = state.postsViewState[state.singlePostId]
   const errorString = viewState && viewState.isError ? viewState.errorString : null
 
