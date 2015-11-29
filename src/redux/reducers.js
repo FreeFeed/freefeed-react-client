80e8ec259a47b94b7450e3c3677c8473b9afec46
Alex Olshansky <i@creagenics.com>
2015-11-29 20:12:18 +0100

    Move scroll-to-top from transition request to transition response
    It makes more sense to scroll on response, when new data is received
    already, instead of jumping to top first and then making user wait
    for the page to load.

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -93,6 +93,7 @@ export function createPostViewState(state = {}, action) {
 const initFeed = {feed: []}
 
 const loadFeedViewState = posts => {
+  scrollTo(0, 0)
   const feed = (posts || []).map(post => post.id)
   return { feed }
 }
@@ -118,6 +119,7 @@ export function feedViewState(state = initFeed, action) {
       return { feed: [postId, ...state.feed] }
     }
     case response(GET_SINGLE_POST): {
+      scrollTo(0, 0)
       const postId = action.request.postId
       return { feed: [postId] }
     }
--- src/redux/route-actions.js
+++ src/redux/route-actions.js
@@ -1,21 +1,16 @@
 import {home, discussions, direct, getUserFeed, getUserComments, getUserLikes, getSinglePost} from './action-creators'
 
-const scrollToTop = value => {
-  scrollTo(0, 0)
-  return value
-}
-
 //query params are strings, so + hack to convert to number
 const getOffset = nextRoute => +nextRoute.location.query.offset || 0
 
 export const routeActions = {
-  'home': next => scrollToTop(home(getOffset(next))),
-  'discussions': next => scrollToTop(discussions(getOffset(next))),
-  'userFeed': next => scrollToTop(getUserFeed(next.params.userName, getOffset(next))),
-  'userComments': next => scrollToTop(getUserComments(next.params.userName, getOffset(next))),
-  'userLikes': next => scrollToTop(getUserLikes(next.params.userName, getOffset(next))),
-  'post': next => scrollToTop(getSinglePost(next.params.postId)),
-  'direct': next => scrollToTop(direct(getOffset(next))),
+  'home': next => home(getOffset(next)),
+  'discussions': next => discussions(getOffset(next)),
+  'userFeed': next => getUserFeed(next.params.userName, getOffset(next)),
+  'userComments': next => getUserComments(next.params.userName, getOffset(next)),
+  'userLikes': next => getUserLikes(next.params.userName, getOffset(next)),
+  'post': next => getSinglePost(next.params.postId),
+  'direct': next => direct(getOffset(next)),
 }
 
 export const bindRouteActions = dispatch => route => next => {
