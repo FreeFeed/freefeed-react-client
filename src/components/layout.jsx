b2ac22bc589591c4d3a29be277d0495ab9431fe7
Alex Olshansky <i@creagenics.com>
2015-12-30 16:01:44 +0100

    Refactor Layout into non-simplified component
    (we'll need this in the next commit)

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -16,50 +16,56 @@ const InternalLayout = ({authenticated, children}) => (
 )
 
 
-const Layout = (props) => (
-  <div className='container'>
-    <div className='row header-row'>
-      <div className='col-md-4'>
-        <div className='header'>
-          <h1 className='title'>
-            <Link to='/'>FreeFeed-beta</Link>
-          </h1>
-        </div>
-      </div>
+class Layout extends React.Component {
+  render() {
+    let props = this.props
 
-      <div className='col-md-8'>
-        <div className='row'>
-          <div className='col-md-6 search-field'>
-            <div className='form-inline'>
-            {/*<input className='form-control input-sm search-input p-search-input' />
-            <button className='btn btn-default btn-sm p-search-action'>Search</button>*/}
+    return (
+      <div className='container'>
+        <div className='row header-row'>
+          <div className='col-md-4'>
+            <div className='header'>
+              <h1 className='title'>
+                <Link to='/'>FreeFeed-beta</Link>
+              </h1>
             </div>
           </div>
-          <div className='col-md-6'>
-            {props.authenticated ? false : (
-              <div className='signin-toolbar'>
-                <Link to='/signin'>Sign In</Link>
+
+          <div className='col-md-8'>
+            <div className='row'>
+              <div className='col-md-6 search-field'>
+                <div className='form-inline'>
+                {/*<input className='form-control input-sm search-input p-search-input' />
+                <button className='btn btn-default btn-sm p-search-action'>Search</button>*/}
+                </div>
               </div>
-            )}
+              <div className='col-md-6'>
+                {props.authenticated ? false : (
+                  <div className='signin-toolbar'>
+                    <Link to='/signin'>Sign In</Link>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <LoaderContainer loading={props.loadingView} fullPage={true}>
+          <div className='row'>
+            <InternalLayout {...props}/>
+            {props.authenticated ? <Sidebar {...props}/> : false}
+          </div>
+        </LoaderContainer>
+
+        <div className='row'>
+          <div className='col-md-12'>
+          <Footer/>
           </div>
         </div>
       </div>
-    </div>
-
-    <LoaderContainer loading={props.loadingView} fullPage={true}>
-      <div className='row'>
-        <InternalLayout {...props}/>
-        {props.authenticated ? <Sidebar {...props}/> : false}
-      </div>
-    </LoaderContainer>
-
-    <div className='row'>
-      <div className='col-md-12'>
-      <Footer/>
-      </div>
-    </div>
-  </div>
-)
+    )
+  }
+}
 
 function select(state) {
   return {
