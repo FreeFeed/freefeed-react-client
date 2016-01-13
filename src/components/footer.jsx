9b212f6126b70fbdddc9370f01c2677dc4c28785
Berkus Decker <berkus@atta-metta.net>
2016-01-13 14:58:43 +0200

    Version 0.5.6

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default (props) => (
   <footer className='footer p-footer'>
-    FreeFeed 0.5.3 beta &copy; 2016<br/>
+    &copy; FreeFeed beta 0.5.6 (January 13, 2016)<br/>
     <a href="https://about.freefeed.net" target="_blank">About</a> | <a href="http://news.freefeed.net" target="_blank">Public News</a> | <a href="https://twitter.com/freefeednet" target="_blank">Twitter</a> | <a href="http://freefeed.reformal.ru/" target="_blank">Reformal: suggest new features</a>
   </footer>
 )
