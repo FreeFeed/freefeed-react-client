9cb8499fa470c7ebb91fa1dde6d14b7464448541
Alex Olshansky <i@creagenics.com>
2016-01-15 23:04:15 +0100

    Refactor: create component <HiddenEntriesToggle/>

--- src/components/feed.jsx
+++ src/components/feed.jsx
@@ -1,6 +1,25 @@
 import React from 'react'
 import Post from './post'
 
+const HiddenEntriesToggle = (props) => {
+  const entriesForm = (props.count > 1 ? 'entries' : 'entry')
+  let label
+
+  if (props.isOpen) {
+    label = `\u25bc Don't show ${props.count} hidden ${entriesForm}`
+  } else {
+    label = `\u25ba Show ${props.count} hidden ${entriesForm}`
+  }
+
+  return (
+    <div className="hidden-posts-toggle">
+      <a onClick={props.toggle}>
+        {label}
+      </a>
+    </div>
+  )
+}
+
 export default (props) => {
   const getEntryComponent = section => post => {
     const isRecentlyHidden = (post.isHidden && (section === 'visible'))
@@ -34,23 +53,17 @@ export default (props) => {
   const visibleEntries = props.visibleEntries.map(getEntryComponent('visible'))
   const hiddenEntries = (props.hiddenEntries || []).map(getEntryComponent('hidden'))
 
-  let toggleLink
-  const entriesForm = (hiddenEntries.length > 1 ? 'entries' : 'entry')
-  if (props.isHiddenRevealed) {
-    const text = <span>Don't show {hiddenEntries.length} hidden {entriesForm}</span>
-    toggleLink = <a onClick={props.toggleHiddenPosts}>&#x25bc; {text}</a>
-  } else {
-    const text = <span>Show {hiddenEntries.length} hidden {entriesForm}</span>
-    toggleLink = <a onClick={props.toggleHiddenPosts}>&#x25ba; {text}</a>
-  }
-
   return (
     <div className="posts">
       {visibleEntries}
 
       {hiddenEntries.length > 0 ? (
         <div>
-          <div className="hidden-posts-toggle">{toggleLink}</div>
+          <HiddenEntriesToggle
+            count={hiddenEntries.length}
+            isOpen={props.isHiddenRevealed}
+            toggle={props.toggleHiddenPosts}/>
+
           {props.isHiddenRevealed ? hiddenEntries : false}
         </div>
       ) : false}
