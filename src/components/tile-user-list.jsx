dfceebd1bd5d50c35b0b7c36154ded6172dc4bfc
Alex Olshansky <i@creagenics.com>
2016-01-30 21:25:04 +0100

    Update TileUserList component (and subs)
    - Sort subscriptions and subscribers by username
    - Display screenName instead of username
    - Make `title` property optional
    - Add optional `size` property (to enable large userpics)

--- src/components/tile-user-list.jsx
+++ src/components/tile-user-list.jsx
@@ -1,28 +1,34 @@
 import React from 'react'
 import {Link} from 'react-router'
+import classnames from 'classnames'
 
 import throbber16 from 'assets/images/throbber-16.gif'
 
-const renderUsers = user => {
-  const profilePictureMediumUrl = user.profilePictureMediumUrl
+const renderUsers = (size) => (user) => {
+  const userpicUrl = (size === 'large') ? user.profilePictureLargeUrl : user.profilePictureMediumUrl
 
   return (
-    <li key={user.id} className='p-user-subscriber'>
+    <li key={user.id}>
       <Link to={`/${user.username}`}>
-        <div className='avatar'>
-          <img src={profilePictureMediumUrl}/>
+        <div className="avatar">
+          <img src={userpicUrl}/>
         </div>
-        <span>{user.username}</span>
+        <span>{user.screenName}</span>
       </Link>
     </li>
   )
 }
 
 const TileList = props => {
-  const users = props.users.map(renderUsers)
+  const users = props.users.map(renderUsers(props.size))
+
+  const listClasses = classnames({
+    'tile-list': true,
+    'large-pics': props.size === 'large'
+  })
 
   return (
-    <ul className='tile-list'>
+    <ul className={listClasses}>
       {users}
     </ul>
   )
@@ -31,18 +37,22 @@ const TileList = props => {
 export default (props) => {
   return (
     <div>
-      <h3 className='p-user-subscribers'>
-        <span>{props.title}&nbsp;</span>
-        {props.users.isPending ? (
-          <span className="comment-throbber">
-            <img width="16" height="16" src={throbber16}/>
-          </span>
-        ) : false}
-      </h3>
-      {props.users.errorString ? (
-        <span className="error-string">{props.users.errorString}</span>
+      {props.title ? (
+        <h3>
+          <span>{props.title} </span>
+          {props.isPending ? (
+            <span className="comment-throbber">
+              <img width="16" height="16" src={throbber16}/>
+            </span>
+          ) : false}
+        </h3>
       ) : false}
-      <TileList users={props.users.payload}/>
+
+      {props.errorString ? (
+        <span className="error-string">{props.errorString}</span>
+      ) : false}
+
+      <TileList users={props.users} size={props.size}/>
     </div>
   )
 }
