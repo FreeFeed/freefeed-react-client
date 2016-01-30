00a45f2f7c57107f8f35d323b70bcf93cbac1690
Alex Olshansky <i@creagenics.com>
2016-01-30 21:34:53 +0100

    Refactor route-actions.js

--- src/redux/route-actions.js
+++ src/redux/route-actions.js
@@ -1,4 +1,18 @@
-import {home, discussions, direct, getUserFeed, getUserComments, getUserLikes, getSinglePost, subscribers, subscriptions, getUserInfo} from './action-creators'
+import {
+  // Private (content depends on current user)
+  home,
+  discussions,
+  direct,
+
+  // Public (content depends on URL params)
+  getUserInfo,
+  getUserFeed,
+  getUserComments,
+  getUserLikes,
+  subscribers,
+  subscriptions,
+  getSinglePost
+} from './action-creators'
 
 //query params are strings, so + hack to convert to number
 const getOffset = nextRoute => +nextRoute.location.query.offset || 0
@@ -10,14 +24,15 @@ const getUserName = nextRoute => {
 export const routeActions = {
   'home': next => home(getOffset(next)),
   'discussions': next => discussions(getOffset(next)),
+  'direct': next => direct(getOffset(next)),
+
+  'getUserInfo': next => getUserInfo(getUserName(next)),
   'userFeed': next => getUserFeed(next.params.userName, getOffset(next)),
   'userComments': next => getUserComments(next.params.userName, getOffset(next)),
   'userLikes': next => getUserLikes(next.params.userName, getOffset(next)),
-  'post': next => getSinglePost(next.params.postId),
-  'direct': next => direct(getOffset(next)),
   'subscribers': next => subscribers(getUserName(next)),
   'subscriptions': next => subscriptions(getUserName(next)),
-  'getUserInfo': next => getUserInfo(getUserName(next))
+  'post': next => getSinglePost(next.params.postId)
 }
 
 export const bindRouteActions = dispatch => route => next => {
