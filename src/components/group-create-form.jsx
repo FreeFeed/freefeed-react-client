619207bb20a7cee96250c1bbda2a724bff754b64
Alex Olshansky <i@creagenics.com>
2016-01-31 14:56:38 +0100

    Add link to the group after it's been created
    Another approach would be to redirect user there automatically.
    I tried that way (in redirectMiddleware), but unfortunately it doesn't
    let all the reducers to finish their jobs and some states were left
    not updated.

--- src/components/group-create-form.jsx
+++ src/components/group-create-form.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import {Link} from 'react-router'
+
 import {preventDefault} from '../utils'
 import throbber16 from 'assets/images/throbber-16.gif'
 
@@ -27,32 +29,41 @@ export default class GroupCreateForm extends React.Component {
 
   render() {
     return (
-      <form onSubmit={preventDefault(this.saveSettings)}>
-        <div className="form-group">
-          <label htmlFor="username">Username:</label>
-          <input id="username" className="form-control" name="username" type="text" value={this.state.username} onChange={this.handleChange('username')}/>
-        </div>
-        <div className="form-group">
-          <label htmlFor="screenName">Screen name:</label>
-          <input id="screenName" className="form-control" name="screenName" type="text" value={this.state.screenName} onChange={this.handleChange('screenName')}/>
-        </div>
-        <div className="form-group">
-          <label htmlFor="description">Description:</label>
-          <textarea id="description" className="form-control" name="description" value={this.state.description} onChange={this.handleChange('description')} maxLength="1500"/>
-        </div>
-        <p>
-          <button className="btn btn-default" type="submit">Create</button>
-          {this.props.status === 'loading' ? (
-            <span className="settings-throbber">
-              <img width="16" height="16" src={throbber16}/>
-            </span>
-          ) : false}
-        </p>
+      <div>
+        {this.props.status !== 'success' ? (
+          <form onSubmit={preventDefault(this.saveSettings)}>
+            <div className="form-group">
+              <label htmlFor="username">Username:</label>
+              <input id="username" className="form-control" name="username" type="text" value={this.state.username} onChange={this.handleChange('username')}/>
+            </div>
+            <div className="form-group">
+              <label htmlFor="screenName">Screen name:</label>
+              <input id="screenName" className="form-control" name="screenName" type="text" value={this.state.screenName} onChange={this.handleChange('screenName')}/>
+            </div>
+            <div className="form-group">
+              <label htmlFor="description">Description:</label>
+              <textarea id="description" className="form-control" name="description" value={this.state.description} onChange={this.handleChange('description')} maxLength="1500"/>
+            </div>
+            <p>
+              <button className="btn btn-default" type="submit">Create</button>
+              {this.props.status === 'loading' ? (
+                <span className="settings-throbber">
+                  <img width="16" height="16" src={throbber16}/>
+                </span>
+              ) : false}
+            </p>
+          </form>
+        ) : false}
+
         {this.props.status === 'success' ? (
-          <div className="alert alert-info" role="alert">Created!</div>
+          <div className="alert alert-info" role="alert">
+            Group has been created.{' '}
+            <Link to={this.props.groupUrl}>Go to the group page »</Link>
+          </div>
         ) : this.props.status === 'error' ? (
           <div className="alert alert-danger" role="alert">{this.props.errorMessage}</div>
         ) : false}
-      </form>)
+      </div>
+    )
   }
 }
