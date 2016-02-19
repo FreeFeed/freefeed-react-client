4c1001f56cf8bf47af1b5a40a398497d3083769b
Alexey Zakhlestin <indeyets@gmail.com>
2016-02-19 18:10:16 +0300

    Replace the link to reformal in footer with a link to the service status dashboard

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,6 +4,6 @@ import {Link} from 'react-router'
 export default (props) => (
   <footer className="footer">
     &copy; FreeFeed 0.6.3 (February 8, 2016)<br/>
-    <Link to="/about">About</Link> | <Link to="/freefeed">News</Link> | <a href="https://twitter.com/freefeednet" target="_blank">Twitter</a> | <a href="http://freefeed.reformal.ru/" target="_blank">Reformal: suggest new features</a>
+    <Link to="/about">About</Link> | <Link to="/freefeed">News</Link> | <a href="https://twitter.com/freefeednet" target="_blank">Twitter</a> | <a href="https://status.freefeed.net/" target="_blank">Status</a>
   </footer>
 )
