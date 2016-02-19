320682ca590b112ce9e6c470fe29d74844b9b1c1
Alex Olshansky <i@creagenics.com>
2016-02-19 22:40:51 +0100

    Refactor: update user settings form
    - <h2> -> <h3>
    - "Screen name" -> "Display name"
    - Rename one form component

--- src/components/settings.jsx
+++ src/components/settings.jsx
@@ -2,23 +2,37 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {updateUser, userSettingsChange, updatePassword, updateUserPhoto} from '../redux/action-creators'
 import UserSettingsForm from './user-settings-form'
-import ChangePasswordForm from './change-password-form'
+import UserChangePasswordForm from './user-change-password-form'
 import UserPhotoForm from './user-photo-form'
 
 const Settings = (props) => (
-  <div className='content'>
-    <div className='box'>
-      <div className='box-header-timeline'>
+  <div className="content">
+    <div className="box">
+      <div className="box-header-timeline">
         Settings
       </div>
-      <div className='box-body'>
-        <UserSettingsForm user={props.user} updateUser={props.updateUser} userSettingsChange={props.userSettingsChange} {...props.userSettingsForm}/>
+      <div className="box-body">
+        <UserSettingsForm
+          user={props.user}
+          updateUser={props.updateUser}
+          userSettingsChange={props.userSettingsChange}
+          {...props.userSettingsForm}/>
+
         <hr/>
-        <ChangePasswordForm {...props.passwordForm} updatePassword={props.updatePassword} />
+
+        <UserChangePasswordForm
+          updatePassword={props.updatePassword}
+          {...props.passwordForm}/>
+
         <hr/>
-        <UserPhotoForm {...props.userPhotoForm} updateUserPhoto={props.updateUserPhoto}/>
-        </div>
+
+        <UserPhotoForm
+          updateUserPhoto={props.updateUserPhoto}
+          {...props.userPhotoForm}/>
+
+        <hr/>
       </div>
+    </div>
   </div>
 )
 
