e655e3d2662675e4feb797216e1e93230c234c4b
Alex Olshansky <i@creagenics.com>
2016-03-03 22:07:40 +0100

    Fix webpack build warning
    To be exact, this one:
    
        WARNING in ./test (?!index).*jsx?
        Module not found: Error: a dependency to an entry point is not allowed
         @ ./test (?!index).*jsx?
    
    Instead of looking for `*.js` files that are not `index.js` (which for some
    reason doesn't work for webpack), the new regexp looks for any `*.js` files
    in subdirectories. (And we don't really need `*.jsx`, right?)

--- test/index.js
+++ test/index.js
@@ -1,4 +1,4 @@
 //webpack context magic to include all tests we've wrote
-var context = require.context('.', true, /(?!index).*jsx?/)
+var context = require.context('.', true, /^\.\/.*\/.*\.js$/)
 context.keys().forEach(context)
 module.exports = context
