a678853a8cab2606cb89d65e99ad308fc290d554
Alex Olshansky <i@creagenics.com>
2016-03-08 23:09:17 +0100

    Update code style in requests.jsx
    Also: "You have no request" -> "You have no requests"

--- src/components/requests.jsx
+++ src/components/requests.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import {connect} from 'react-redux'
-
 import {Link} from 'react-router'
+
 import {acceptGroupRequest, rejectGroupRequest,
         acceptUserRequest, rejectUserRequest} from '../redux/action-creators'
 import {tileUserListFactory, WITH_REQUEST_HANDLES} from './tile-user-list'
+
 const TileList = tileUserListFactory({type: WITH_REQUEST_HANDLES})
 
 const renderRequestsToGroup = (accept, reject) => (groupRequests) => {
@@ -14,9 +15,10 @@ const renderRequestsToGroup = (accept, reject) => (groupRequests) => {
   return (
     <div key={groupRequests.id}>
       <h3>{groupRequests.screenName}</h3>
-      <TileList users={groupRequests.requests}
-                       acceptRequest={acceptGroupRequest}
-                       rejectRequest={rejectGroupRequest}/>
+      <TileList
+        users={groupRequests.requests}
+        acceptRequest={acceptGroupRequest}
+        rejectRequest={rejectGroupRequest}/>
     </div>
   )
 }
@@ -31,32 +33,33 @@ const RequestsHandler = (props) => {
       </div>
       <div className='box-body'>
         <div><Link to={`/${props.username}`}>{props.username}</Link> › Requests</div>
-        {props.overallRequestsCount == 0
-        ? <p>You have no request</p>
-        : <div>
-            {props.feedRequests && props.feedRequests.length
-            ? <div>
+        {props.overallRequestsCount == 0 ? (
+          <p>You have no requests</p>
+        ) : (
+          <div>
+            {props.feedRequests && props.feedRequests.length ? (
+              <div>
                 <h3>Requests to your feed</h3>
-                <TileList users={props.feedRequests}
-                          acceptRequest={props.acceptUserRequest}
-                          rejectRequest={props.rejectUserRequest}/>
+                <TileList
+                  users={props.feedRequests}
+                  acceptRequest={props.acceptUserRequest}
+                  rejectRequest={props.rejectUserRequest}/>
               </div>
-            : false}
+            ) : false}
 
-            {groupRequests
-            ? <div>
+            {groupRequests ? (
+              <div>
                 {groupRequests}
               </div>
-            : false}
-          </div>}
+            ) : false}
+          </div>
+        )}
       </div>
-      <div className='box-footer'></div>
     </div>
   )
 }
 
 function selectState(state, ownProps) {
-
   const boxHeader = state.boxHeader
   const username = ownProps.params.userName
 
