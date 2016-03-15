11cb3d46240cbea8be6ceee112992a607d7ca223
Berkus Decker <berkus@atta-metta.net>
2016-03-15 10:05:54 +0200

    Managing subscriptions and unsubscriptions

--- src/services/realtime.js
+++ src/services/realtime.js
@@ -2,6 +2,33 @@ import {api as apiConfig} from '../config'
 import {getToken} from './auth'
 import io from 'socket.io-client'
 
+const dummyPost = {
+  getBoundingClientRect: _ => ({top:0})
+}
+
+const scrollCompensator = dispatchAction => (...actionParams) => {
+  //we hope that markup will remain the same — best tradeoff between this and code all over components
+  const postCommentNodes = [...document.querySelectorAll('.post, .comment')]
+
+  const firstVisible = postCommentNodes.filter(element => element.getBoundingClientRect().top > 0)[0]
+
+  const nearestTopIndex = postCommentNodes.indexOf(firstVisible) - 1
+
+  const nearestTop = postCommentNodes[nearestTopIndex] || dummyPost
+
+  const topBefore = nearestTop.getBoundingClientRect().top
+
+  //here we're dispatching, so render is called internally and after call we have new page
+  const res = dispatchAction(...actionParams)
+
+  const topAfter = nearestTop.getBoundingClientRect().top
+
+  if (topAfter !== topBefore) {
+    scrollBy(0, topAfter - topBefore)
+  }
+  return res
+}
+
 const bindSocketLog = socket => eventName => socket.on(eventName, data => console.log(`socket ${eventName}`, data))
 
 const bindSocketActionsLog = socket => events => events.forEach(bindSocketLog(socket))
@@ -14,7 +41,7 @@ const eventsToLog = [
 ]
 
 const bindSocketEventHandlers = socket => eventHandlers => {
-  Object.keys(eventHandlers).forEach((event) => socket.on(event, eventHandlers[event]))
+  Object.keys(eventHandlers).forEach((event) => socket.on(event, scrollCompensator(eventHandlers[event])))
 }
 
 const openSocket = _ => io.connect(`${apiConfig.host}/`, {query: `token=${getToken()}`})
@@ -30,12 +57,14 @@ export function init(eventHandlers){
   let subscribe
 
   return {
-    changeSubscription: newSubscription => {
+    unsubscribe: _ => {
       if (subscription){
         console.log('unsubscribing from ', subscription)
         socket.emit('unsubscribe', subscription)
         socket.off('reconnect', subscribe)
       }
+    },
+    subscribe: newSubscription => {
       subscription = newSubscription
       console.log('subscribing to ', subscription)
       subscribe = () => socket.emit('subscribe', subscription)
@@ -44,38 +73,4 @@ export function init(eventHandlers){
     },
     disconnect: _ => socket.disconnect()
   }
-}
-
-const dummyPost = {
-  getBoundingClientRect: _ => ({top:0})
-}
-
-export const scrollCompensator = dispatchAction => (...actionParams) => {
-  //we hope that markup will remain the same — best tradeoff between this and code all over components
-  const postCommentNodes = [...document.querySelectorAll('.post, .comment')]
-
-  const firstVisible = postCommentNodes.filter(element => element.getBoundingClientRect().top > 0)[0]
-
-  const nearestTopIndex = postCommentNodes.indexOf(firstVisible) - 1
-
-  const nearestTop = postCommentNodes[nearestTopIndex] || dummyPost
-
-  const topBefore = nearestTop.getBoundingClientRect().top
-
-  const heightBefore = document.body.offsetHeight
-
-  //here we're dispatching, so render is called internally and after call we have new page
-  const res = dispatchAction(...actionParams)
-
-  const topAfter = nearestTop.getBoundingClientRect().top
-
-  const heightAfter = document.body.offsetHeight
-
-  const shouldCompensateRealtimeEffect = topAfter !== topBefore
-
-  if (shouldCompensateRealtimeEffect) {
-    scrollBy(0, heightAfter - heightBefore)
-  }
-  return res
-
 }
