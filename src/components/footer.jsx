8e2e8f56c8d95a793caa0dd848ab8d6523a87a4b
Berkus Decker <berkus@atta-metta.net>
2016-03-19 11:13:25 +0200

    Add Development link to footer.

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,6 +4,6 @@ import {Link} from 'react-router'
 export default (props) => (
   <footer className="footer">
     &copy; FreeFeed 0.8.0 (March 19, 2016)<br/>
-    <Link to="/about">About</Link> | <Link to="/freefeed">News</Link> | <a href="https://twitter.com/freefeednet" target="_blank">Twitter</a> | <a href="https://status.freefeed.net/" target="_blank">Status</a>
+    <Link to="/about">About</Link> | <Link to="/freefeed">News</Link> | <a href="https://twitter.com/freefeednet" target="_blank">Twitter</a> | <a href="https://status.freefeed.net/" target="_blank">Status</a> | <a href="https://dev.freefeed.net/" target="_blank">Development</a>
   </footer>
 )
