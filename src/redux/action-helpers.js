806637c10b700c234d3a8cb87648642cb53c6110
Alex Olshansky <i@creagenics.com>
2016-04-06 00:51:17 +0200

    Refactor userChangeActions

--- src/redux/action-helpers.js
+++ src/redux/action-helpers.js
@@ -1,4 +1,8 @@
-import {HOME, DISCUSSIONS, GET_USER_FEED, GET_USER_COMMENTS, GET_USER_LIKES, DIRECT,SUBSCRIBE, UNSUBSCRIBE} from './action-types';
+import {
+  HOME, DISCUSSIONS, DIRECT, GET_USER_FEED, GET_USER_COMMENTS, GET_USER_LIKES,
+  SIGN_UP, WHO_AM_I, SUBSCRIBE, UNSUBSCRIBE,
+  UPDATE_USER, UPDATE_FRONTEND_PREFERENCES, UPDATE_FRONTEND_REALTIME_PREFERENCES
+} from './action-types';
 
 export const request = (type) =>`${type}_REQUEST`;
 export const response = (type) => `${type}_RESPONSE`;
@@ -12,7 +16,7 @@ export const isFeedRequest = action => feedRequests.indexOf(action.type) !== -1;
 export const isFeedResponse = action => feedResponses.indexOf(action.type) !== -1;
 export const isFeedFail = action => feedFails.indexOf(action.type) !== -1;
 
-export const userChangeActions = [SUBSCRIBE, UNSUBSCRIBE];
+export const userChangeActions = [SIGN_UP, WHO_AM_I, SUBSCRIBE, UNSUBSCRIBE, UPDATE_USER, UPDATE_FRONTEND_PREFERENCES, UPDATE_FRONTEND_REALTIME_PREFERENCES];
 export const userChangeResponses = userChangeActions.map(response);
 export const isUserChangeResponse = action => userChangeResponses.indexOf(action.type) !== -1;
 
