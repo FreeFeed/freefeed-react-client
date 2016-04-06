20fd803ef469afb9c3f30a835342d71d74db51dc
Alex Olshansky <i@creagenics.com>
2016-04-06 22:59:46 +0200

    Refactor: rename UserPhotoForm -> UserPictureForm

--- src/components/user-picture-form.jsx
+++ src/components/user-picture-form.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {preventDefault} from '../utils';
 
-export default class UserPhotoForm extends React.Component {
+export default class UserPictureForm extends React.Component {
   render() {
     return (
       <form onSubmit={preventDefault(this.updatePhoto)}>
