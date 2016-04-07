c99115f97ffe9fc5f241ceac5925416aa7d8b872
Alex Olshansky <i@creagenics.com>
2016-04-07 21:35:08 +0200

    Add current image and uploading progress to UserPictureForm

--- src/components/user-picture-form.jsx
+++ src/components/user-picture-form.jsx
@@ -1,30 +1,45 @@
 import React from 'react';
+
 import {preventDefault} from '../utils';
+import throbber16 from 'assets/images/throbber-16.gif';
 
 export default class UserPictureForm extends React.Component {
+  savePicture = () => {
+    const newFile = this.refs.pictureFile.files[0];
+    if (newFile && this.props.status !== 'loading') {
+      this.props.updateUserPicture(newFile);
+    }
+  }
+
   render() {
     return (
-      <form onSubmit={preventDefault(this.updatePhoto)}>
-        <h3>Update profile picture</h3>
-        <div className='form-group'>
-          <input type='file' ref='photo'/>
+      <form onSubmit={preventDefault(this.savePicture)}>
+        <h3>Profile picture</h3>
+
+        <div className="form-group avatar">
+          <img src={this.props.user.profilePictureLargeUrl} width="75" height="75"/>
         </div>
-        <p>
-          <button className='btn btn-default p-settings-updateprofilepicture'>Update</button>
-        </p>
-        {this.props.success ?
-          (<div className='alert alert-info p-settings-alert' role='alert'>
-              <span id='error-message'>Your photo has been updated</span>
-            </div>) : false}
-        {this.props.error ?
-          (<div className='alert alert-danger p-settings-alert' role='alert'>
-              <span id='error-message'>{this.props.errorText}</span>
-            </div>) : false}
-      </form>);
-  }
-  updatePhoto = () => {
-    if (!this.props.isSaving) {
-      this.props.updateUserPhoto(this.refs.photo.files[0]);
-    }
+
+        <div className="form-group">
+          <input type="file" ref="pictureFile"/>
+        </div>
+
+        <div className="form-group">
+          <button className="btn btn-default" type="submit">Update</button>
+
+          {this.props.status === 'loading' ? (
+            <span className="settings-throbber">
+              <img width="16" height="16" src={throbber16}/>
+            </span>
+          ) : false}
+        </div>
+
+        {this.props.status === 'success' ? (
+          <div className="alert alert-info" role="alert">Profile picture has been updated</div>
+        ) : this.props.status === 'error' ? (
+          <div className="alert alert-danger" role="alert">{this.props.errorMessage}</div>
+        ) : false}
+      </form>
+    );
   }
 }
