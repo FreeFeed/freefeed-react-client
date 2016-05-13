8cf93093889646b55a5ec6c701836b5785d61d93
David Mzareulyan <david@hiero.ru>
2016-05-13 17:53:55 +0300

    dev-server script for Windows

---
+++ dev-server.cmd
@@ -0,0 +1,21 @@
+@echo off
+
+set PORT=5783
+
+if not exist .\node_modules3\*.* (
+  echo Installing required NPM modules...
+  npm install
+  echo Done!
+  echo
+) 
+
+set DEV=1
+set LIVERELOAD=1
+
+./node_modules/.bin/webpack-dev-server ^
+  --config webpack.config.js ^
+  --port %PORT% ^
+  --host 0.0.0.0 ^
+  --output-public-path "http://localhost:%PORT%/" ^
+  --colors ^
+  --hot
