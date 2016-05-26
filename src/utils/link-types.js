aded91b6adb53d3b1d559e522b054fbe6b01c985
David Mzareulyan <david@hiero.ru>
2016-05-26 18:10:23 +0300

    Use arrow detection from ff-url-finder 2.3

--- src/utils/link-types.js
+++ src/utils/link-types.js
@@ -3,8 +3,9 @@ export const AT_LINK = 'atLink';
 export const LOCAL_LINK = 'localLink';
 export const EMAIL = 'email';
 export const HASHTAG = 'hashTag';
+export const ARROW = 'arrow';
 
-const linkTypes = [LINK, AT_LINK, LOCAL_LINK, EMAIL, HASHTAG];
+const linkTypes = [LINK, AT_LINK, LOCAL_LINK, EMAIL, HASHTAG, ARROW];
 
 export function isLink({type}) {
   return linkTypes.indexOf(type) !== -1;
