dbe54a962e0c00b379aff220e436a3a1f517e4ff
Alexey Zakhlestin <indeyets@gmail.com>
2016-07-03 18:04:16 +0300

    bump version

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router';
 
 export default (props) => (
   <footer className="footer">
-    &copy; FreeFeed 0.8.13 (June 29, 2016)<br/>
+    &copy; FreeFeed 1.0.0 (July 3, 2016)<br/>
     <Link to="/about">About</Link> | <Link to="/freefeed">News</Link> | <a href="https://twitter.com/freefeednet" target="_blank">Twitter</a> | <a href="https://status.freefeed.net/" target="_blank">Status</a> | <a href="https://dev.freefeed.net/" target="_blank">Development</a>
   </footer>
 );
