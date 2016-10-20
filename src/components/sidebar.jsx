c595455a323579a1d955d41c6b9747066555784a
Alexey Zakhlestin <indeyets@gmail.com>
2016-10-20 10:46:20 +0300

    Give name to SideBar component

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -5,105 +5,106 @@ import {preventDefault} from '../utils';
 import UserName from './user-name';
 import RecentGroups from './recent-groups';
 
-export default ({user, signOut, recentGroups}) => (
-  <div className='col-md-3 sidebar'>
-    <div className='logged-in'>
-      <div className='avatar'>
-        <Link to={`/${user.username}`} ><img src={user.profilePictureMediumUrl} width="50" height="50"/></Link>
-      </div>
-
-      <div className='user'>
-        <div className='author'>
-          <UserName user={user} display={user.screenName}/>
+const SideBar = ({user, signOut, recentGroups}) => {
+  return (
+    <div className='col-md-3 sidebar'>
+      <div className='logged-in'>
+        <div className='avatar'>
+          <Link to={`/${user.username}`} ><img src={user.profilePictureMediumUrl} width="50" height="50"/></Link>
         </div>
-        <div>
-          <Link to='/settings'>settings</Link>
-          &nbsp;-&nbsp;
-          <a onClick={preventDefault(signOut)}>sign out</a>
+
+        <div className='user'>
+          <div className='author'>
+            <UserName user={user} display={user.screenName}/>
+          </div>
+          <div>
+            <Link to='/settings'>settings</Link>
+            &nbsp;-&nbsp;
+            <a onClick={preventDefault(signOut)}>sign out</a>
+          </div>
         </div>
       </div>
-    </div>
 
-    <div className='box'>
-      <div className='box-header-friends'>
-        Friends
+      <div className='box'>
+        <div className='box-header-friends'>
+          Friends
+        </div>
+        <div className='box-body'>
+          <ul>
+            <li className='p-home'><Link to='/'>Home</Link></li>
+            <li className='p-direct-messages'><Link to='/filter/direct'>Direct messages</Link></li>
+            <li className='p-my-discussions'><Link to='/filter/discussions'>My discussions</Link></li>
+          </ul>
+        </div>
+        <div className='box-footer'>
+          <Link to={`/friends`}>Browse/edit friends</Link>
+        </div>
       </div>
-      <div className='box-body'>
-        <ul>
-          <li className='p-home'><Link to='/'>Home</Link></li>
-          <li className='p-direct-messages'><Link to='/filter/direct'>Direct messages</Link></li>
-          <li className='p-my-discussions'><Link to='/filter/discussions'>My discussions</Link></li>
-        </ul>
-      </div>
-      <div className='box-footer'>
-        <Link to={`/friends`}>Browse/edit friends</Link>
-      </div>
-    </div>
 
-    <div className='box'>
-      <div className='box-header-search'>
-        Search
+      <div className='box'>
+        <div className='box-header-search'>
+          Search
+        </div>
+        <div className='box-body'>
+          <ul>
+            <li><Link to='/search'>FreeFeed search</Link></li>
+            <li><Link to={{ pathname: "/search", query: { qs: `"@${user.username}"` } }}>Vanity search</Link></li>
+            <li><Link to='/filter/best_of'>Best of FreeFeed</Link></li>
+          </ul>
+        </div>
       </div>
-      <div className='box-body'>
-        <ul>
-          <li><Link to='/search'>FreeFeed search</Link></li>
-          <li><Link to={{ pathname: "/search", query: { qs: `"@${user.username}"` } }}>Vanity search</Link></li>
-          <li><Link to='/filter/best_of'>Best of FreeFeed</Link></li>
-        </ul>
-      </div>
-    </div>
 
-    <div className='box'>
-      <div className='box-header-groups'>
-        Groups
+      <div className='box'>
+        <div className='box-header-groups'>
+          Groups
+        </div>
+        <div className='box-body'>
+          <RecentGroups recentGroups={recentGroups}/>
+        </div>
+        <div className='box-footer'>
+          <Link to='/groups'>Browse/edit groups</Link>
+        </div>
       </div>
-      <div className='box-body'>
-        <RecentGroups recentGroups={recentGroups}/>
-      </div>
-      <div className='box-footer'>
-        <Link to='/groups'>Browse/edit groups</Link>
-      </div>
-    </div>
 
-    <div className='box'>
-      <div className='box-header-groups'>
-        Info
-      </div>
-      <div className='box-body'>
-        <ul>
-          <li><Link to='/freefeed'>News</Link></li>
-          <li><Link to='/support'>Support</Link></li>
-        </ul>
-      </div>
-      <div className='box-footer'>
+      <div className='box'>
+        <div className='box-header-groups'>
+          Info
+        </div>
+        <div className='box-body'>
+          <ul>
+            <li><Link to='/freefeed'>News</Link></li>
+            <li><Link to='/support'>Support</Link></li>
+          </ul>
+        </div>
+        <div className='box-footer'>
 
+        </div>
       </div>
-    </div>
 
-    <div className='box'>
-      <div className='box-header-groups'>
-        Bookmarklet
+      <div className='box'>
+        <div className='box-header-groups'>
+          Bookmarklet
+        </div>
+        <div className='box-footer'>
+          Once added to your toolbar, this button will let you share web pages on your FreeFeed.
+          You can even attach thumbnails of images from the page you share!
+        </div>
+        <div className='box-footer'>
+          Click and drag
+          {' '}
+          <a className="bookmarklet-button" href="BOOKMARKLET_PLACEHOLDER" onClick={preventDefault(() => false)}>Share on FreeFeed</a>
+          {' '}
+          to&nbsp;your toolbar.
+        </div>
       </div>
-      <div className='box-footer'>
-        Once added to your toolbar, this button will let you share web pages on your FreeFeed.
-        You can even attach thumbnails of images from the page you share!
-      </div>
-      <div className='box-footer'>
-        Click and drag
-        {' '}
-        <a className="bookmarklet-button" href="BOOKMARKLET_PLACEHOLDER" onClick={preventDefault(() => false)}>Share on FreeFeed</a>
-        {' '}
-        to&nbsp;your toolbar.
-      </div>
-    </div>
 
-    <div className='box'>
-      <div className='box-header-groups'>
-        Coin Jar
-      </div>
-      <div className='box-footer'>
-        <p style={{marginBottom: '10px'}}>Subscribe to Freefeed today! Arrangement is plain and simple — you wire funds to Freefeed, it gets better every week.</p>
-        <span style={{display: 'block', marginLeft: 'auto', marginRight: 'auto'}}>
+      <div className='box'>
+        <div className='box-header-groups'>
+          Coin Jar
+        </div>
+        <div className='box-footer'>
+          <p style={{marginBottom: '10px'}}>Subscribe to Freefeed today! Arrangement is plain and simple — you wire funds to Freefeed, it gets better every week.</p>
+          <span style={{display: 'block', marginLeft: 'auto', marginRight: 'auto'}}>
           <form action='https://www.paypal.com/cgi-bin/webscr' method='post' target='_top'>
             <input type='hidden' name='cmd' value='_s-xclick'/>
             <input type='hidden' name='hosted_button_id' value='DRR5XU73QLD7Y'/>
@@ -130,14 +131,17 @@ export default ({user, signOut, recentGroups}) => (
             <img alt='' src='https://www.paypalobjects.com/en_US/i/scr/pixel.gif' width='1' height='1' style={{display: 'none !important'}}/>
           </form>
         </span>
-        <form action='https://www.paypal.com/cgi-bin/webscr' method='post' target='_top' id='singlePayPalPayment'>
-          <input type='hidden' name='cmd' value='_s-xclick'/>
-          <input type='hidden' name='hosted_button_id' value='HMVYD6GEWNWH8'/>
-          <input type='image' src='https://www.paypalobjects.com/webstatic/en_US/i/buttons/PP_logo_h_100x26.png' width='0' height='0' name='submit' alt='PayPal - The safer, easier way to pay online!'/>
-          <img alt='' src='https://www.paypalobjects.com/en_US/i/scr/pixel.gif' width='1' height='1' style={{display: 'none !important'}}/>
-        </form>
-        <p style={{marginBottom: '10px'}}>Or <span onClick={() => document.forms["singlePayPalPayment"].submit()} style={{textDecoration:'underline', cursor:'pointer'}}>send a one-time payment first&nbsp;→</span></p>
+          <form action='https://www.paypal.com/cgi-bin/webscr' method='post' target='_top' id='singlePayPalPayment'>
+            <input type='hidden' name='cmd' value='_s-xclick'/>
+            <input type='hidden' name='hosted_button_id' value='HMVYD6GEWNWH8'/>
+            <input type='image' src='https://www.paypalobjects.com/webstatic/en_US/i/buttons/PP_logo_h_100x26.png' width='0' height='0' name='submit' alt='PayPal - The safer, easier way to pay online!'/>
+            <img alt='' src='https://www.paypalobjects.com/en_US/i/scr/pixel.gif' width='1' height='1' style={{display: 'none !important'}}/>
+          </form>
+          <p style={{marginBottom: '10px'}}>Or <span onClick={() => document.forms["singlePayPalPayment"].submit()} style={{textDecoration:'underline', cursor:'pointer'}}>send a one-time payment first&nbsp;→</span></p>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
+
+export default SideBar;
