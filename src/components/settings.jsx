728244df69e76707fa225ba900f2b47e82ba7d1e
David Mzareulyan <david@hiero.ru>
2016-10-28 17:14:46 +0300

    Reset forms when user navigates from the Settings page

--- src/components/settings.jsx
+++ src/components/settings.jsx
@@ -1,50 +1,59 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {updateUser, userSettingsChange, updateFrontendPreferences, updatePassword, updateUserPicture, toggleRealtime} from '../redux/action-creators';
+import {updateUser, userSettingsChange, updateFrontendPreferences, updatePassword, updateUserPicture, toggleRealtime, resetSettingsForms} from '../redux/action-creators';
 import UserSettingsForm from './user-settings-form';
 import UserFrontendPreferencesForm from './user-frontend-preferences-form';
 import UserChangePasswordForm from './user-change-password-form';
 import UserPictureForm from './user-picture-form';
 
-const Settings = (props) => (
-  <div className="content">
-    <div className="box">
-      <div className="box-header-timeline">
-        Settings
+class Settings extends React.Component {
+  componentWillUnmount() {
+    this.props.resetSettingsForms();
+  }
+
+  render() {
+    const props = this.props;
+    return (
+      <div className="content">
+        <div className="box">
+          <div className="box-header-timeline">
+            Settings
+          </div>
+          <div className="box-body">
+            <UserSettingsForm
+              user={props.user}
+              updateUser={props.updateUser}
+              userSettingsChange={props.userSettingsChange}
+              {...props.userSettingsForm}/>
+
+            <hr/>
+
+            <UserFrontendPreferencesForm
+              userId={props.user.id}
+              preferences={props.user.frontendPreferences}
+              updateFrontendPreferences={props.updateFrontendPreferences}
+              {...props.frontendPreferencesForm}/>
+
+            <hr/>
+
+            <UserChangePasswordForm
+              updatePassword={props.updatePassword}
+              {...props.passwordForm}/>
+
+            <hr/>
+
+            <UserPictureForm
+              user={props.user}
+              updateUserPicture={props.updateUserPicture}
+              {...props.userPictureForm}/>
+
+            <hr/>
+          </div>
+        </div>
       </div>
-      <div className="box-body">
-        <UserSettingsForm
-          user={props.user}
-          updateUser={props.updateUser}
-          userSettingsChange={props.userSettingsChange}
-          {...props.userSettingsForm}/>
-
-        <hr/>
-
-        <UserFrontendPreferencesForm
-          userId={props.user.id}
-          preferences={props.user.frontendPreferences}
-          updateFrontendPreferences={props.updateFrontendPreferences}
-          {...props.frontendPreferencesForm}/>
-
-        <hr/>
-
-        <UserChangePasswordForm
-          updatePassword={props.updatePassword}
-          {...props.passwordForm}/>
-
-        <hr/>
-
-        <UserPictureForm
-          user={props.user}
-          updateUserPicture={props.updateUserPicture}
-          {...props.userPictureForm}/>
-
-        <hr/>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 function mapStateToProps(state) {
   return {
@@ -65,6 +74,7 @@ function mapDispatchToProps(dispatch) {
     updatePassword: (...args) => dispatch(updatePassword(...args)),
     updateUserPicture: (...args) => dispatch(updateUserPicture(...args)),
     toggleRealtime: () => dispatch(toggleRealtime()),
+    resetSettingsForms: () => dispatch(resetSettingsForms())
   };
 }
 
