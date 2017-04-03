39aabcdf09b60d59c186fe3d49c33f4ee9c1748b
Victor Suzdalev <victor.suzdalev@gmail.com>
2017-04-03 17:21:41 +0300

    Fix setState on unmounted components

--- src/components/user-name.jsx
+++ src/components/user-name.jsx
@@ -48,12 +48,14 @@ class UserName extends React.Component {
       isHovered: false,
       isCardOpen: false
     };
+    this.enterUserName = this.enterUserName.bind(this);
+    this.leaveUserName = this.leaveUserName.bind(this);
   }
 
   enterUserName() {
     this.setState({isHovered: true});
 
-    setTimeout(() => {
+    this.enterTimeout = setTimeout(() => {
       if (this.state.isHovered) {
         const {bottom, left} = ReactDOM.findDOMNode(this).getBoundingClientRect();
         const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
@@ -69,7 +71,7 @@ class UserName extends React.Component {
   leaveUserName() {
     this.setState({isHovered: false});
 
-    setTimeout(() => {
+    this.leaveTimeout = setTimeout(() => {
       if (!this.state.isHovered) {
         this.setState({isCardOpen: false});
       }
@@ -104,6 +106,15 @@ class UserName extends React.Component {
         ) : false}
       </div>
     );
+  }
+
+  componentWillUnmount() {
+    if (this.enterTimeout) {
+      clearTimeout(this.enterTimeout);
+    }
+    if (this.leaveTimeout) {
+      clearTimeout(this.leaveTimeout);
+    }
   }
 }
 
