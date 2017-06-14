0b0f14501891f870cd1c3a75edb3d1516ff55834
Yury Shevchenko <unique.login@gmail.com>
2017-06-14 23:17:21 +0800

    Better class names for post likes

--- src/components/post-likes.jsx
+++ src/components/post-likes.jsx
@@ -4,11 +4,11 @@ import {preventDefault} from '../utils';
 import UserName from './user-name';
 
 const renderLike = (item, i, items) => (
-  <li key={item.id} className="like">
+  <li key={item.id} className="post-like">
     {item.id !== 'more-likes' ? (
       <UserName user={item}/>
     ) : (
-      <a className="more-likes-link" onClick={preventDefault(item.showMoreLikes)}>{item.omittedLikes} other people</a>
+      <a className="more-post-likes-link" onClick={preventDefault(item.showMoreLikes)}>{item.omittedLikes} other people</a>
     )}
 
     {i < items.length - 2 ? (
@@ -39,9 +39,9 @@ export default ({likes, showMoreLikes, post}) => {
   const renderedLikes = likeList.map(renderLike);
 
   return (
-    <div className="likes">
+    <div className="post-likes">
       <i className="fa fa-heart icon"></i>
-      <ul className="likes-list">{renderedLikes}</ul>
+      <ul className="post-likes-list">{renderedLikes}</ul>
     </div>
   );
 };
