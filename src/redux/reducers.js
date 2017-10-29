1a30279a1e9c4bf38084e384f94aa16ef4bfdaf4
Victor Suzdalev <victor.suzdalev@gmail.com>
2017-10-29 22:03:25 +0300

    Hint users to sign in or sign up if they started with wrong page

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -68,7 +68,7 @@ export function signInForm(state={username:'', password:'', error:'', loading: f
       };
     }
     case ActionTypes.UNAUTHENTICATED: {
-      return {...state, error: (action.payload || {}).err, loading: false };
+      return {...state, error: (action.payload || {}).err, loading: false, requireAuth: false };
     }
     case ActionTypes.SIGN_IN_EMPTY: {
       return {...state, error: 'Enter login and password', loading: false };
@@ -80,7 +80,7 @@ export function signInForm(state={username:'', password:'', error:'', loading: f
       return {...state, loading: false };
     }
     case ActionTypes.REQUIRE_AUTHENTICATION: {
-      return {...state, error: 'You need to sign in'};
+      return {...state, requireAuth: true};
     }
   }
   return state;
