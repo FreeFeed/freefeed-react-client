491be143946dee4b71d516c6d89a38755f1c2c83
Dmitry Vdovin <voidxnull@gmail.com>
2018-02-18 16:59:06 +0200

    Add openOauthPopup util function

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,7 +1,10 @@
+import { isArray } from 'lodash';
+
 import { getCookie, setCookie, localStorage } from '../utils/';
 import config from '../config';
 
 const authConfig = config.auth;
+const apiConfig = config.api;
 const NAME = `${authConfig.tokenPrefix}authToken`;
 const EXP_DAYS = 365;
 const PATH = '/';
@@ -21,4 +24,61 @@ export function getPersistedUser() {
 export function persistUser(user) {
   return user ? localStorage.setItem(authConfig.userStorageKey, JSON.stringify(user)) :
     localStorage.removeItem(authConfig.userStorageKey);
+}
+
+export function openOauthPopup(uri) {
+  return new Promise((resolve, reject) => {
+    function oauthListener(event) {
+      if (event.origin !== apiConfig.host) {
+        // eslint-disable-next-line no-console
+        console.warn('Message with unsupported origin', event);
+      }
+
+      window.removeEventListener('message', oauthListener, false);
+
+      const { data } = event;
+
+      if (data.error) {
+        reject(new Error(data.error));
+        return;
+      }
+
+      resolve(data);
+    }
+
+    window.addEventListener('message', oauthListener, false);
+
+    const popup = window.open(
+      uri,
+      '_blank',
+      // 'centerscreen,toolbar=0,scrollbars=1,status=1,resizable=1,location=0,menuBar=0,width=800,height=1000'
+    );
+
+    if (popup) {
+      popup.focus();
+    } else {
+      reject(new Error('Could not open authentication window'));
+      window.removeEventListener('message', oauthListener, false);
+    }
+  });
+}
+
+export function openOauthAuthPopup(provider) {
+  checkOauthProvider(provider);
+
+  const uri = `${apiConfig.host}/v2/oauth/${provider}`;
+  return openOauthPopup(uri);
+}
+
+export function openOauthLinkPopup(provider) {
+  checkOauthProvider(provider);
+
+  const uri = `${apiConfig.host}/v2/oauth/${provider}/link?authToken=${getToken()}`;
+  return openOauthPopup(uri);
+}
+
+function checkOauthProvider(provider) {
+  if (!isArray(authConfig.oauth) || !authConfig.oauth.includes(provider)) {
+    throw Error(`Unsupported OAuth provider "${provider}"`);
+  }
 }
