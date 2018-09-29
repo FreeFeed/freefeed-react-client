f8b9a9dbb3497b889dd4c20f3acfba66d334d550
David Mzareulyan <david@hiero.ru>
2018-09-29 11:11:08 +0300

    Add 'social-text-tokenizer' for text parsing

---
+++ src/utils/parse-text.js
@@ -0,0 +1,48 @@
+import { URL } from 'url';
+import { includes } from 'lodash';
+import {
+  withText,
+  combine,
+  hashTags,
+  emails,
+  mentions,
+  links,
+  arrows,
+  Link as TLink,
+} from 'social-text-tokenizer';
+
+import config from '../config';
+
+export class Link extends TLink {
+  url = null;
+
+  constructor(link) {
+    super(link.offset, link.text);
+    this.url = new URL(this.href);
+  }
+
+  get isLocal() {
+    return includes(config.siteDomains, this.url.hostname);
+  }
+
+  get localURI() {
+    return this.url.pathname + this.url.search + this.url.hash;
+  }
+}
+
+const tokenize = withText(combine(hashTags, emails, mentions, links, arrows));
+
+const enhanceLinks = (token) => (token instanceof TLink) ? new Link(token) : token;
+
+export const parseText = (text) => tokenize(text).map(enhanceLinks);
+
+export function getFirstLinkToEmbed(text) {
+  return parseText(text)
+    .filter((token) => (
+      token instanceof Link
+      && !token.isLocal
+      && /^https?:\/\//i.test(token.text)
+      && text.charAt(token.offset - 1) !== '!'
+    ))
+    .map((it) => it.href)[0];
+}
