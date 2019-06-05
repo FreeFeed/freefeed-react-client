7a9b417c85b33f2cddb08c91228bae87e00578f1
David Mzareulyan <david@hiero.ru>
2019-06-05 12:55:44 +0300

    Update getFeedName function to handle all types of feeds

--- src/redux/action-helpers.js
+++ src/redux/action-helpers.js
@@ -10,13 +10,18 @@ export const response = (type) => `${type}_RESPONSE`;
 export const fail = (type) => `${type}_FAIL`;
 
 export const feedGeneratingActions = [HOME, DISCUSSIONS, GET_USER_FEED, GET_USER_COMMENTS, GET_USER_LIKES, DIRECT, GET_SEARCH, GET_BEST_OF, GET_EVERYTHING, GET_SUMMARY, GET_USER_SUMMARY, MEMORIES, GET_USER_MEMORIES];
+const userFeedGeneratingActions = [GET_USER_FEED, GET_USER_COMMENTS, GET_USER_LIKES, GET_USER_SUMMARY, GET_USER_MEMORIES];
+
 export const isFeedGeneratingAction = (action) => feedGeneratingActions.includes(action.type);
+const isUserFeedGeneratingAction = (action) => userFeedGeneratingActions.includes(action.type);
+
 export const feedRequests = feedGeneratingActions.map(request);
 export const feedResponses = feedGeneratingActions.map(response);
 export const feedFails = feedGeneratingActions.map(fail);
 export const isFeedRequest = (action) => feedRequests.includes(action.type);
 export const isFeedResponse = (action) => feedResponses.includes(action.type);
 export const isFeedFail = (action) => feedFails.includes(action.type);
+const isUserFeedRequest = (action) => userFeedGeneratingActions.map(request).includes(action.type);
 
 export const userChangeActions = [SIGN_UP, WHO_AM_I, SUBSCRIBE, UNSUBSCRIBE, UPDATE_USER, UPDATE_USER_PREFERENCES];
 export const userChangeResponses = userChangeActions.map(response);
@@ -26,9 +31,16 @@ export function requiresAuth(action) {
   return action.apiRequest && !action.nonAuthRequest;
 }
 
+const unRequest = (type) => type.replace(/_REQUEST$/, '');
+
 export function getFeedName(action) {
-  if (action.type === request(HOME) || action.type === HOME) {
-    return HOME;
+  if (!isFeedGeneratingAction(action) && !isFeedRequest(action)) {
+    return "unknown";
   }
-  return `${GET_USER_FEED}_${action.payload.username}`;
+
+  if (isUserFeedGeneratingAction(action) || isUserFeedRequest(action)) {
+    return `${unRequest(action.type)}_${action.payload.username}`;
+  }
+
+  return unRequest(action.type);
 }
