46cc5c33a6f3c44560cdfd879f49403d1d7194c6
David Mzareulyan <david@hiero.ru>
2019-06-05 12:49:28 +0300

    Replace .indexOf to .includes in action helpers

--- src/redux/action-helpers.js
+++ src/redux/action-helpers.js
@@ -10,17 +10,17 @@ export const response = (type) => `${type}_RESPONSE`;
 export const fail = (type) => `${type}_FAIL`;
 
 export const feedGeneratingActions = [HOME, DISCUSSIONS, GET_USER_FEED, GET_USER_COMMENTS, GET_USER_LIKES, DIRECT, GET_SEARCH, GET_BEST_OF, GET_EVERYTHING, GET_SUMMARY, GET_USER_SUMMARY, MEMORIES, GET_USER_MEMORIES];
-export const isFeedGeneratingAction = (action) => feedGeneratingActions.indexOf(action.type) != -1;
+export const isFeedGeneratingAction = (action) => feedGeneratingActions.includes(action.type);
 export const feedRequests = feedGeneratingActions.map(request);
 export const feedResponses = feedGeneratingActions.map(response);
 export const feedFails = feedGeneratingActions.map(fail);
-export const isFeedRequest = (action) => feedRequests.indexOf(action.type) !== -1;
-export const isFeedResponse = (action) => feedResponses.indexOf(action.type) !== -1;
-export const isFeedFail = (action) => feedFails.indexOf(action.type) !== -1;
+export const isFeedRequest = (action) => feedRequests.includes(action.type);
+export const isFeedResponse = (action) => feedResponses.includes(action.type);
+export const isFeedFail = (action) => feedFails.includes(action.type);
 
 export const userChangeActions = [SIGN_UP, WHO_AM_I, SUBSCRIBE, UNSUBSCRIBE, UPDATE_USER, UPDATE_USER_PREFERENCES];
 export const userChangeResponses = userChangeActions.map(response);
-export const isUserChangeResponse = (action) => userChangeResponses.indexOf(action.type) !== -1;
+export const isUserChangeResponse = (action) => userChangeResponses.includes(action.type);
 
 export function requiresAuth(action) {
   return action.apiRequest && !action.nonAuthRequest;
