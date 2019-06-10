d8e1f2544a0646a5d0ac04422841d222e23adf0d
Alexey Zakhlestin <indeyets@gmail.com>
2019-06-10 10:43:06 +0300

    Disable dev-modules in production

--- webpack/rules.js
+++ webpack/rules.js
@@ -53,11 +53,11 @@ class RuleGenerator {
               "@babel/proposal-class-properties",
               "@babel/proposal-do-expressions",
               "@babel/proposal-object-rest-spread",
-              '@babel/transform-runtime',
+              this.opts.dev && '@babel/transform-runtime',
               // ['@babel/plugin-transform-modules-commonjs', {
               //   "noInterop": true,
               // }],
-              "react-hot-loader/babel",
+              this.opts.hot && "react-hot-loader/babel",
               !this.opts.dev && ['transform-react-remove-prop-types', { mode: 'remove', removeImport: true, additionalLibraries: ["react-style-proptype"] }],
               !this.opts.dev && ["@babel/transform-react-constant-elements"],
               !this.opts.dev && ["@babel/transform-react-inline-elements"],
