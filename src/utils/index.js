48aab327bf2b2349e91c08247b3577f2f5c4f4e2
Alexey Zakhlestin <indeyets@gmail.com>
2019-09-06 11:26:48 +0300

    Upgrade ESLint

--- src/utils/index.js
+++ src/utils/index.js
@@ -125,7 +125,7 @@ try {
 const localStorageShim = {
   _data: {},
   setItem(id, val) { return this._data[id] = String(val); },
-  getItem(id) { return this._data.hasOwnProperty(id) ? this._data[id] : null; },
+  getItem(id) { return Object.prototype.hasOwnProperty.call(this._data, id) ? this._data[id] : null; },
   removeItem(id) { return delete this._data[id]; },
   clear() { return this._data = {}; }
 };
