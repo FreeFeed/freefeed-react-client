ee7a4acbbc079e58dc0458c63560bdeff3f2601d
David Mzareulyan <david@hiero.ru>
2019-10-08 13:27:50 +0300

    Add utility functions to work with popups via promises

---
+++ src/services/popup.js
@@ -0,0 +1,48 @@
+export function centeredPopup(url, { name = 'popup', width = 500, height = 500 } = {}) {
+  return window.open(
+    url,
+    name,
+    [
+      `height=${height}`,
+      `width=${width}`,
+      `top=${Math.round((screen.height - height) / 2.5)}`,
+      `left=${Math.round((screen.width - width) / 2)}`,
+    ].join(','),
+  );
+}
+
+export function popupAsPromise(popup, { origins = [location.origin] } = {}) {
+  return new Promise((resolve, reject) => {
+    function listener(event) {
+      if (event.source !== popup || !origins.includes(event.origin)) {
+        return;
+      }
+      window.removeEventListener('message', listener, false);
+      const { data } = event;
+      if (data.error) {
+        reject(new Error(data.error));
+        return;
+      }
+      resolve(data);
+    }
+
+    window.addEventListener('message', listener, false);
+
+    if (popup) {
+      popup.focus();
+    } else {
+      reject(new Error('Could not open window'));
+      window.removeEventListener('message', listener, false);
+      return;
+    }
+
+    (function checkClose() {
+      if (popup.closed) {
+        // Do this reject just to be safe
+        setTimeout(() => reject(new Error('Window was suddenly closed')), 250);
+        return;
+      }
+      setTimeout(checkClose, 250);
+    })();
+  });
+}
