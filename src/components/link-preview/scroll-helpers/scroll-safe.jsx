f0eea51d5c75d880fcc1cca310654dcfaf1bd40f
David Mzareulyan <david@hiero.ru>
2019-10-18 20:14:46 +0300

    Rewrite the ScrollSafe wrapper
    It fixes the incompatibility with the modern React

--- src/components/link-preview/scroll-helpers/scroll-safe.jsx
+++ src/components/link-preview/scroll-helpers/scroll-safe.jsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import _ from 'lodash';
 
 import ResizeTracker from './resize-tracker';
 import FoldableContent from './foldable-content';
 import ScrollCompensator from './scroll-compensator';
 
 /**
- * ScrollSafe is a decorator for react classes. It provides:
+ * ScrollSafe is a decorator for react components that provides:
  *   1) automatic scroll compensation for the nested component and
  *   2) Fold/Expand UI for the tall content.
  *
- * ScrollSafe can be used as a decorator:
- *   @ScrollSafe
- *   class Foo extends React.Component {
- *
- * …as a decorator with options:
- *   @ScrollSafe(options)
- *   class Foo extends React.Component {
- *
- * …or as a regular function:
+ * Usage:
  *   ScrollSafe(Foo) or ScrollSafe(Foo, options)
  *
  * Options is a plain object with the following fields:
@@ -28,21 +19,8 @@ import ScrollCompensator from './scroll-compensator';
  * When you set 'trackResize' to false you still can manually signal to the
  * ScrollSafe wrapper about your component resize - see contentResized() function in 'events.jsx'.
  */
-export default function ScrollSafe(arg1, arg2) {
-  const defaultOptions = {
-    foldable: true,
-    trackResize: true,
-  };
-
-  if (_.isPlainObject(arg1)) {
-    // Call as a decorator with parameters: @ScrollSafe(opts)
-    return (Child) => ScrollSafe(Child, arg1);
-  }
-
-  const Child = arg1;
-  const { foldable, trackResize } = { ...defaultOptions, ...(arg2 || {}) };
-
-  const foo = function(props) {
+export default function ScrollSafe(Child, { foldable = true, trackResize = true } = {}) {
+  return function ScrollSafe(props) {
     let content = <Child {...props} />;
     if (trackResize) {
       content = <ResizeTracker>{content}</ResizeTracker>;
@@ -52,7 +30,4 @@ export default function ScrollSafe(arg1, arg2) {
     }
     return <ScrollCompensator>{content}</ScrollCompensator>;
   };
-  foo.displayName = 'ScrollSafe';
-
-  return foo;
 }
