60da5fd3dd68d064978cff8a4583fbc8b3a7eff8
David Mzareulyan <david@hiero.ru>
2019-12-14 14:56:54 +0300

    Update lazy loading for lightbox and image sorting components

--- src/components/post-attachment-image-container.jsx
+++ src/components/post-attachment-image-container.jsx
@@ -1,45 +1,28 @@
 import pt from 'prop-types';
 import React from 'react';
 import classnames from 'classnames';
-import Loadable from 'react-loadable';
 
 import { faChevronCircleRight } from '@fortawesome/free-solid-svg-icons';
 import ImageAttachment from './post-attachment-image';
 import { Icon } from './fontawesome-icons';
+import { lazyComponent } from './lazy-component';
 
 const bordersSize = 4;
 const spaceSize = 8;
 const arrowSize = 24;
 
-const ImageAttachmentsLightbox = Loadable({
-  loading: ({ error, pastDelay }) => {
-    if (error) {
-      console.error(`Cannot load 'post-attachment-image-lightbox'`, error); // eslint-disable-line no-console
-      return <div style={{ color: 'red' }}>Cannot load lightbox. Please try again.</div>;
-    }
-    if (pastDelay) {
-      return (
-        <div className="lightbox-loading">
-          <span>Loading lightbox...</span>
-        </div>
-      );
-    }
-    return null;
-  },
-  loader: () => import('./post-attachment-image-lightbox'),
-  delay: 500,
-  timeout: 10000,
+const ImageAttachmentsLightbox = lazyComponent(() => import('./post-attachment-image-lightbox'), {
+  fallback: (
+    <div className="lightbox-loading">
+      <span>Loading lightbox...</span>
+    </div>
+  ),
+  errorMessage: "Couldn't load lightbox component",
 });
 
-const Sortable = Loadable({
-  loading: ({ error }) => {
-    if (error) {
-      return <div>Cannot load Sortable component</div>;
-    }
-    return <div>Loading component...</div>;
-  },
-  loader: () => import('react-sortablejs'),
-  delay: 500,
+const Sortable = lazyComponent(() => import('react-sortablejs'), {
+  fallback: <div>Loading component...</div>,
+  errorMessage: "Couldn't load Sortable component",
 });
 
 export default class ImageAttachmentsContainer extends React.Component {
