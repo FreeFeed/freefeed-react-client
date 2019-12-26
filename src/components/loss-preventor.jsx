daf1c9ca3287f2872d9d9ae8824a196084609d24
Atari <jager@djem.ru>
2019-12-26 22:14:49 +0300

    Loss is now prevented on router navigation, too

--- src/components/loss-preventor.jsx
+++ src/components/loss-preventor.jsx
@@ -1,29 +1,42 @@
-import { Children, useEffect, useMemo } from 'react';
+import { Children, useEffect, useMemo, useRef } from 'react';
+import { withRouter } from 'react-router';
 
 const getChildrenValue = (children) =>
   Children.map(children, (child) => child.props.value || '').join(';');
 
 function LossPreventor(props) {
   const initialValue = useMemo(() => getChildrenValue(props.children), []);
+  const childrenRef = useRef();
+  childrenRef.current = props.children;
+
+  useEffect(() => {
+    const { router, routes } = props;
+    router.setRouteLeaveHook(routes[routes.length - 1], () => {
+      if (getChildrenValue(childrenRef.current) !== initialValue) {
+        if (!confirm('Leave the page? Changes you made may not be saved.')) {
+          return false;
+        }
+      }
+    });
+    return () => router.setRouteLeaveHook(routes[routes.length - 1], () => {});
+  }, []);
 
   useEffect(() => {
     const reloadWatcher = (e) => {
-      if (getChildrenValue(props.children) === initialValue) {
+      if (getChildrenValue(childrenRef.current) === initialValue) {
         delete e.returnValue;
       } else {
         e.preventDefault();
         e.returnValue = '';
       }
     };
-
     window.addEventListener('beforeunload', reloadWatcher);
-
     return () => {
       window.removeEventListener('beforeunload', reloadWatcher);
     };
-  }, [props.children]);
+  }, []);
 
   return props.children;
 }
 
-export default LossPreventor;
+export default withRouter(LossPreventor);
