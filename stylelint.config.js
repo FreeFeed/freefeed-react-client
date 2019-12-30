4b79ead3b44a7a8b563376f4945a7c07fdd0c0f2
David Mzareulyan <david@hiero.ru>
2019-12-30 11:49:23 +0300

    Add hook and style that helps create dropdown menus

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,5 +2,6 @@ module.exports = {
   extends: ['stylelint-prettier/recommended', 'stylelint-config-recommended-scss'],
   rules: {
     'no-descending-specificity': null,
+    'selector-pseudo-class-no-unknown': [true, { ignorePseudoClasses: ['global'] }],
   },
 };
