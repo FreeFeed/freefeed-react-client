cc1b3193dc024fe94e1a261e6e9a53298ed3f5e6
David Mzareulyan <david@hiero.ru>
2020-01-11 17:20:34 +0300

    Extract profile picture edit form to the separate component

--- src/components/settings/forms/profile-picture.jsx
+++ src/components/settings/forms/profile-picture.jsx
@@ -12,17 +12,22 @@ export default function ProfilePictureForm() {
   const dispatch = useDispatch();
   const pictureURL = useSelector((state) => state.user.profilePictureLargeUrl);
   const pictureStatus = useSelector((state) => state.settingsForms.updatePictureStatus);
+  const onUpdate = useCallback((file) => dispatch(updateUserPicture(file)), [dispatch]);
 
+  return <PictureEditForm {...{ pictureURL, pictureStatus, onUpdate }} />;
+}
+
+export function PictureEditForm({ pictureURL, pictureStatus, onUpdate }) {
   const fileInput = useMemo(() => {
     const input = document.createElement('input');
     input.type = 'file';
     input.accept = 'image/jpeg, image/png, image/gif';
     input.addEventListener('input', () => {
-      dispatch(updateUserPicture(input.files[0]));
+      onUpdate(input.files[0]);
       input.value = ''; // reset input state
     });
     return input;
-  }, [dispatch]);
+  }, [onUpdate]);
 
   const choosePictureFile = useCallback(() => pictureStatus.loading || fileInput.click(), [
     fileInput,
@@ -48,7 +53,7 @@ export default function ProfilePictureForm() {
             onClick={choosePictureFile}
             disabled={pictureStatus.loading}
           >
-            {pictureStatus.loading ? <>Updating picture&hellip;</> : <>Update picture</>}
+            {pictureStatus.loading ? <>Updating picture</> : <>Update picture</>}
           </button>{' '}
           {pictureStatus.loading && <Throbber />}
         </p>
