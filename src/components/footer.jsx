f3de29a8d0efec5e6bed9122c1233a8610a6a301
Alexey Zakhlestin <indeyets@gmail.com>
2020-04-06 20:10:14 +0300

    Bump version

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router';
 
 export default () => (
   <footer className="footer">
-    &copy; FreeFeed 1.76.0 (Mar, 29, 2020)
+    &copy; FreeFeed 1.77.0 (Apr, 6, 2020)
     <br />
     <Link to="/about">About</Link>
     {' | '}
