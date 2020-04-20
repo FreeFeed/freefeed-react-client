fb2e71174c3180a2b5fc509ad5d37a2627ddd223
David Mzareulyan <david@hiero.ru>
2020-04-20 19:56:59 +0300

    Redesign the search page, add Advanced Search form to it

--- src/components/search-feed.jsx
+++ src/components/search-feed.jsx
@@ -1,30 +1,66 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react';
+import { connect, useSelector } from 'react-redux';
+import { faChevronDown, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+
 import { joinPostData, postActions } from './select-utils';
 import Feed from './feed';
 import PaginatedView from './paginated-view';
 import { lazyComponent } from './lazy-component';
+import { useSearchQuery } from './hooks/search-query';
+import { useBool } from './hooks/bool';
+import { ButtonLink } from './button-link';
+import { Icon } from './fontawesome-icons';
 
-const SearchHelp = lazyComponent(() => import('./search-help.md'), {
-  fallback: <div>Loading search help...</div>,
-  errorMessage: "Couldn't load search help",
-});
-
-const FeedHandler = (props) => (
-  <div className="box">
-    <div className="box-header-timeline">{props.boxHeader}</div>
-    {props.entries.length ? (
-      <PaginatedView {...props}>
-        <Feed {...props} />
-      </PaginatedView>
-    ) : (
-      <div className="box-body">
-        <SearchHelp />
-      </div>
-    )}
-    <div className="box-footer" />
-  </div>
+const SearchFormAdvanced = lazyComponent(
+  () => import('./search-form-advanced').then((m) => ({ default: m.SearchFormAdvanced })),
+  {
+    fallback: <div>Loading form...</div>,
+    errorMessage: "Couldn't load search form",
+  },
 );
+
+function FeedHandler(props) {
+  const queryString = useSearchQuery();
+  const pageIsLoading = useSelector((state) => state.routeLoadingState);
+  const [advFormVisible, setAdvFormVisible] = useBool(!queryString);
+
+  useEffect(() => void (pageIsLoading && setAdvFormVisible(false)), [
+    pageIsLoading,
+    setAdvFormVisible,
+  ]);
+
+  return (
+    <div className="box">
+      <div className="box-header-timeline">{props.boxHeader}</div>
+      <div className="box-body" style={{ marginTop: '1em' }}>
+        {queryString && (
+          <>
+            <p>
+              {pageIsLoading ? 'Searching' : 'Seach results'} for: <strong>{queryString}</strong>
+            </p>
+            {props.entries.length === 0 && !pageIsLoading && (
+              <p>No results found. Try a different query.</p>
+            )}
+            <p>
+              <ButtonLink onClick={setAdvFormVisible}>
+                <Icon icon={advFormVisible ? faChevronDown : faChevronRight} /> Advanced search
+                options
+              </ButtonLink>
+            </p>
+          </>
+        )}
+        {(!queryString || advFormVisible) && <SearchFormAdvanced />}
+        {props.entries.length > 0 && <hr />}
+      </div>
+      {props.entries.length > 0 && (
+        <PaginatedView {...props}>
+          <Feed {...props} />
+        </PaginatedView>
+      )}
+      <div className="box-footer" />
+    </div>
+  );
+}
 
 function selectState(state) {
   const { authenticated, boxHeader, highlightTerms, timelines, user } = state;
