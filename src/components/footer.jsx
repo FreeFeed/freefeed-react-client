e9fa290a146105ec8a8501a4258fd5763c077ce4
Dmitry Sumin <dmitry.sumin@gmail.com>
2020-06-23 21:40:38 +0300

    Returns status link back

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -17,6 +17,10 @@ export default () => (
     {' | '}
     <Link to="/about/stats">Stats</Link>
     {' | '}
+    <a href="https://status.freefeed.net/" target="_blank">
+      Status
+    </a>
+    {' | '}
     <a href="https://github.com/FreeFeed" target="_blank">
       GitHub
     </a>
