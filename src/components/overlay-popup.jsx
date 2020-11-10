0cc9c3b34205d014e3ade4def4e199779918a1b4
David Mzareulyan <david@hiero.ru>
2020-11-10 13:19:39 +0300

    Allow OverlayPopup to close on Esc key or by clicking on the overlay

--- src/components/overlay-popup.jsx
+++ src/components/overlay-popup.jsx
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Portal } from 'react-portal';
 import FocusTrap from 'focus-trap-react';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { KEY_ESCAPE } from 'keycode-js';
 
 import styles from './overlay-popup.module.scss';
 import { Icon } from './fontawesome-icons';
+import { withEventListener } from './hooks/sub-unsub';
 
-export function OverlayPopup({ close, children }) {
+export function OverlayPopup({
+  close,
+  children,
+  // Close by Escape or by clicking on the overlay
+  easyClose = true,
+}) {
+  const panel = useRef();
+  const content = useRef();
+
+  useEffect(
+    () =>
+      withEventListener(
+        panel.current,
+        'keydown',
+        ({ keyCode }) => easyClose && keyCode === KEY_ESCAPE && close(),
+      ),
+    [close, easyClose],
+  );
+
+  useEffect(
+    () =>
+      withEventListener(
+        panel.current,
+        'click',
+        ({ target }) => easyClose && !content.current.contains(target) && close(),
+      ),
+    [close, easyClose],
+  );
+
   return (
     <Portal>
       <FocusTrap>
-        <div className={styles.popup}>
-          <div className={styles.content}>
+        <div className={styles.popup} ref={panel}>
+          <div className={styles.content} ref={content}>
             {close && (
               <button onClick={close} className={styles.closeBtn}>
                 <Icon icon={faTimes} aria-label="Close" />
