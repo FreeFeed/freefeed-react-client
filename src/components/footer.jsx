365bba8a01eb04d42377fc87675815aa12d49f71
Alexey Zakhlestin <indeyets@gmail.com>
2021-03-03 10:50:00 +0300

    Back to dev

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
   return (
     <footer className="footer">
       <p role="navigation">
-        &copy; FreeFeed 1.95.0 (March 03, 2021)
+        &copy; FreeFeed 1.96.0 (Not released)
         <br />
         <Link to="/about">About</Link>
         {' | '}
