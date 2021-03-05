12163f0e860635a85e35adfb8d2c4e00ba2e9f08
Alexey Zakhlestin <indeyets@gmail.com>
2021-03-05 12:58:26 +0300

    Release 1.95.3

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
   return (
     <footer className="footer">
       <p role="navigation">
-        &copy; FreeFeed 1.95.3 (Not released)
+        &copy; FreeFeed 1.95.3 (March 5, 2021)
         <br />
         <Link to="/about">About</Link>
         {' | '}
