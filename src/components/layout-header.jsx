dea215ca39d732805d13674e85a9e36ebe9a9930
David Mzareulyan <david@hiero.ru>
2021-03-31 15:21:48 +0300

    Add 'Clear search form' button, collapse form on blur

--- src/components/layout-header.jsx
+++ src/components/layout-header.jsx
@@ -1,6 +1,6 @@
 /* global CONFIG */
 import { IndexLink, Link, withRouter } from 'react-router';
-import { faBars, faSearch, faSignInAlt, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faSearch, faSignInAlt, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import cn from 'classnames';
 import { KEY_ESCAPE } from 'keycode-js';
@@ -22,6 +22,8 @@ export const LayoutHeader = withRouter(function LayoutHeader({ router }) {
   const authenticated = useSelector((state) => state.authenticated);
 
   const [searchExpanded, setSearchExpanded] = useState(false);
+  const [query, setQuery] = useState('');
+  const onQueryChange = useCallback(({ target }) => setQuery(target.value), []);
 
   const fullSearchForm = isWideScreen;
   const compactSearchForm = !fullSearchForm;
@@ -34,31 +36,23 @@ export const LayoutHeader = withRouter(function LayoutHeader({ router }) {
 
   const initialQuery = useInitialQuery(router);
   const input = useRef(null);
-  useEffect(() => void (input.current && (input.current.value = initialQuery)), [initialQuery]);
+  useEffect(() => void setQuery(initialQuery), [initialQuery]);
 
   const onSubmit = useCallback(
     (e) => {
       e.preventDefault();
-      const query = input.current?.value.trim() || '';
-      if (query !== '') {
-        router.push(`/search?qs=${encodeURIComponent(query)}`);
+      const q = query.trim();
+      if (q !== '') {
+        router.push(`/search?qs=${encodeURIComponent(q)}`);
         input.current.blur();
-        closeSearchForm();
       }
     },
-    [router, closeSearchForm],
+    [router, query],
   );
 
-  const onKeyDown = useCallback(
-    (e) => {
-      if (e.keyCode === KEY_ESCAPE) {
-        input.current.blur();
-        input.current.value = initialQuery;
-        closeSearchForm();
-      }
-    },
-    [initialQuery, closeSearchForm],
-  );
+  const onKeyDown = useCallback((e) => e.keyCode === KEY_ESCAPE && input.current.blur(), []);
+
+  const clearSearchForm = useCallback(() => (setQuery(''), input.current.focus()), []);
 
   const onFocus = useCallback(() => isNarrowScreen && onSearchPage && setSearchExpanded(true), [
     isNarrowScreen,
@@ -67,34 +61,40 @@ export const LayoutHeader = withRouter(function LayoutHeader({ router }) {
 
   const doOpenSidebar = useCallback(() => dispatch(openSidebar(true)), [dispatch]);
 
+  const focusHandlers = useDebouncedFocus({
+    onFocus,
+    onBlur: closeSearchForm,
+  });
+
   const searchForm = (
     <form className={styles.searchForm} action="/search" onSubmit={onSubmit}>
-      <input
-        className={styles.searchInput}
-        type="text"
-        name="qs"
-        ref={input}
-        placeholder="Search request"
-        autoFocus={collapsibleSearchForm}
-        defaultValue={initialQuery}
-        onKeyDown={onKeyDown}
-        onFocus={onFocus}
-      />
-      {compactSearchForm && <Icon icon={faSearch} className={styles.searchIcon} />}
+      <span className={styles.searchInputContainer} {...focusHandlers}>
+        <input
+          className={styles.searchInput}
+          type="text"
+          name="q"
+          ref={input}
+          placeholder="Search request"
+          autoFocus={collapsibleSearchForm}
+          value={query}
+          onChange={onQueryChange}
+          onKeyDown={onKeyDown}
+        />
+        {compactSearchForm && <Icon icon={faSearch} className={styles.searchIcon} />}
+        <button
+          type="button"
+          className={cn(styles.clearSearchButton, styles.compactButton)}
+          aria-label="Clear search form"
+          title="Clear search form"
+          onClick={clearSearchForm}
+          tabIndex={-1}
+        >
+          <Icon icon={faTimesCircle} />
+        </button>
+      </span>
       {fullSearchForm && (
         <button type="submit" className={styles.searchButton}>
           Search
-        </button>
-      )}
-      {collapsibleSearchForm && (
-        <button
-          type="button"
-          className={cn(styles.closeSearchButton, styles.compactButton)}
-          aria-label="Close search form"
-          title="Close search form"
-          onClick={closeSearchForm}
-        >
-          <Icon icon={faTimes} />
         </button>
       )}
     </form>
@@ -196,4 +196,26 @@ function useInitialQuery(router) {
         return '';
     }
   }, [router.routes, router.params, router.location]);
+}
+
+function useDebouncedFocus({ onFocus: onFocusOrig, onBlur: onBlurOrig }, interval = 100) {
+  const focusTimer = useRef(0);
+  const blurTimer = useRef(0);
+
+  const cleanup = useCallback(() => {
+    window.clearTimeout(blurTimer.current);
+    window.clearTimeout(focusTimer.current);
+  }, []);
+  useEffect(() => () => cleanup(), [cleanup]);
+
+  const onFocus = useCallback(() => {
+    cleanup();
+    focusTimer.current = window.setTimeout(onFocusOrig, interval);
+  }, [cleanup, onFocusOrig, interval]);
+  const onBlur = useCallback(() => {
+    cleanup();
+    blurTimer.current = window.setTimeout(onBlurOrig, interval);
+  }, [cleanup, onBlurOrig, interval]);
+
+  return { onFocus, onBlur };
 }
