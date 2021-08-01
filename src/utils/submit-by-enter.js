a8bc99a3927c5b0752ec18d9412da4249d72a937
Konstantin Grusha <konstantin@grusha.name>
2021-08-01 12:47:48 +0300

    Add Alt as another newline modifier in comments

--- src/utils/submit-by-enter.js
+++ src/utils/submit-by-enter.js
@@ -3,16 +3,16 @@ import { KEY_RETURN } from 'keycode-js';
 /**
  * Returns the keydown handler that handles submit by Enter in text fields.
  *
- * The Enter press acts as submit unless the Shift key is pressed or the text
+ * The Enter press acts as submit unless the Shift or Alt key is pressed, or the text
  * cursor is at the end of text and the text ends with space. The space+Enter
- * pattern allows mobile users (that cannot use Shift) to insert soft returns to
+ * pattern allows mobile users (that cannot use Shift or Alt) to insert soft returns to
  * text.
  *
  * @param {Function} submitFn submit action function
  */
 export function submitByEnter(submitFn) {
   return function (event) {
-    if (event.keyCode !== KEY_RETURN || event.shiftKey) {
+    if (event.keyCode !== KEY_RETURN || event.shiftKey || event.altKey) {
       return;
     }
 
