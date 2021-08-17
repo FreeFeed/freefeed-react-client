93ff395c0a36cfaf85a6b4eb0c756a467510ad7d
Yury Shevchenko <unique.login@gmail.com>
2021-08-17 13:19:44 -0700

    Initial commit for support of video-type attachments

---
+++ src/components/post-attachment-video.jsx
@@ -0,0 +1,74 @@
+import { PureComponent } from 'react';
+import { faFileVideo, faPlayCircle } from '@fortawesome/free-regular-svg-icons';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+import { formatFileSize } from '../utils';
+import { ButtonLink } from './button-link';
+import { Icon } from './fontawesome-icons';
+
+class VideoAttachment extends PureComponent {
+  state = {
+    isOpen: false,
+  };
+
+  handleClickOnRemoveAttachment = () => {
+    this.props.removeAttachment(this.props.id);
+  };
+
+  toggleOpen = () => {
+    this.setState({ isOpen: true });
+  };
+
+  render() {
+    const { props } = this;
+    const { isOpen } = this.state;
+    const formattedFileSize = formatFileSize(props.fileSize);
+
+    const title = `${props.fileName} (${formattedFileSize})`;
+
+    const thumbnailStyle = {
+      width: props.imageSizes.t?.w,
+      height: props.imageSizes.t?.h,
+      backgroundImage: `url(${props.imageSizes.t?.url})`,
+    };
+
+    return (
+      <div className="attachment" role="figure" aria-label={`Video attachment ${title}`}>
+        {isOpen ? (
+          <div>
+            <video title={title} autoPlay controls>
+              <source src={props.url} />
+              Your browser does not support HTML5 video tag.
+            </video>
+          </div>
+        ) : (
+          <ButtonLink
+            onClick={this.toggleOpen}
+            className="video-attachment-thumb"
+            title="Click to play video"
+            style={thumbnailStyle}
+          >
+            <Icon icon={faPlayCircle} className="video-attachment-click-to-play" />
+          </ButtonLink>
+        )}
+        <div>
+          <a href={props.url} title={title} target="_blank">
+            <Icon icon={faFileVideo} className="attachment-icon" />
+            <span>{title}</span>
+          </a>
+
+          {props.isEditing && (
+            <Icon
+              icon={faTimes}
+              className="remove-attachment"
+              title="Remove video file"
+              onClick={this.handleClickOnRemoveAttachment}
+            />
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default VideoAttachment;
---
+++ test/jest/post-attachments.test.js
@@ -0,0 +1,82 @@
+/* global describe, it, expect */
+import { render } from '@testing-library/react';
+
+import PostAttachments from '../../src/components/post-attachments';
+
+const renderPostAttachments = (props = {}) => {
+  return render(<PostAttachments {...props} />);
+};
+
+describe('PostAttachments', () => {
+  it('Renders an empty attachments container', () => {
+    const { asFragment } = renderPostAttachments();
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('Displays all post attachment types', () => {
+    const image1 = {
+      id: 'im1',
+      mediaType: 'image',
+      fileName: 'CAT.JPG',
+      fileSize: 200000,
+      thumbnailUrl: 'https://thumbnail/CAT.JPG',
+      url: 'https://media/CAT.JPG',
+      imageSizes: {
+        t: {
+          w: 400,
+          h: 300,
+        },
+        o: {
+          w: 2000,
+          h: 1500,
+        },
+      },
+    };
+
+    const image2 = {
+      id: 'im2',
+      mediaType: 'image',
+      fileName: 'food.jpg',
+      fileSize: 2000,
+      thumbnailUrl: 'https://thumbnail/food.jpg',
+      url: 'https://media/food.jpg',
+      imageSizes: {
+        o: {
+          w: 2000,
+          h: 1500,
+        },
+      },
+    };
+
+    const video1 = {
+      id: 'vi1',
+      mediaType: 'general',
+      fileName: 'sunrise.mp4',
+      fileSize: 123456789,
+      url: 'https://media/sunrise.mp4',
+    };
+
+    const audio1 = {
+      id: 'au1',
+      mediaType: 'audio',
+      fileName: 'wonderwall.mp3',
+      artist: 'Oasis',
+      title: 'Wonderwall',
+      fileSize: 1234567,
+      url: 'https://media/wonderwall.mp3',
+    };
+
+    const general1 = {
+      id: 'ge1',
+      mediaType: 'general',
+      fileName: 'rfc.pdf',
+      fileSize: 50000,
+      url: 'https://media/rfc.pdf',
+    };
+
+    const { asFragment } = renderPostAttachments({
+      attachments: [video1, image1, general1, image2, audio1],
+    });
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
