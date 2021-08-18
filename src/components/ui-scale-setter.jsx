3b82354b918e11891bc1cd25278c623446bbddc4
David Mzareulyan <david@hiero.ru>
2021-08-18 14:45:35 +0300

    Try to take the system font size into account

--- src/components/ui-scale-setter.jsx
+++ src/components/ui-scale-setter.jsx
@@ -1,8 +1,32 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 export function UIScaleSetter() {
   const scale = useSelector((state) => state.uiScale);
-  useEffect(() => (document.documentElement.style.fontSize = `${(10 * scale) / 100}px`), [scale]);
+  const [systemScale, setSystemScale] = useState(null);
+
+  useEffect(() => {
+    // Create a test element (that is subject to font-boosting) to measure the
+    // OS/browser font size
+    const testElement = document.body.appendChild(document.createElement('div'));
+    testElement.style.fontSize = '1rem';
+    // We must postpone the measurement so that the browser has time to apply
+    // font boosting
+    setTimeout(() => {
+      const systemTextSize = parseInt(getComputedStyle(testElement).fontSize);
+      // The default font size in most browsers is 16px, but we define body's
+      // font size as 14/16 rem's.
+      setSystemScale(isFinite(systemTextSize) ? systemTextSize / 16 : 1);
+      testElement.parentElement.removeChild(testElement);
+    }, 0);
+  }, []);
+
+  useEffect(() => {
+    if (systemScale === null) {
+      return;
+    }
+    document.documentElement.style.fontSize = `${(16 * systemScale * scale) / 100}px`;
+  }, [systemScale, scale]);
+
   return null;
 }
