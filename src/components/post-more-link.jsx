f2f316c5a68242df0ed6ef294bdd28381435a93a
Yury Shevchenko <unique.login@gmail.com>
2021-09-15 23:43:02 -0700

    Make "more" menu of posts more mobile-friendly

--- src/components/post-more-link.jsx
+++ src/components/post-more-link.jsx
@@ -1,10 +1,12 @@
-import { useCallback } from 'react';
+import { useCallback, useEffect } from 'react';
 import { Portal } from 'react-portal';
+import { intentToScroll } from '../services/unscroll';
 
 import { confirmFirst } from '../utils';
 import { canonicalURI } from '../utils/canonical-uri';
 import { CLOSE_ON_CLICK_OUTSIDE } from './hooks/drop-down';
 import { useDropDownKbd } from './hooks/drop-down-kbd';
+import { useMediaQuery } from './hooks/media-query';
 import { useServerInfo } from './hooks/server-info';
 import { PostMoreMenu } from './post-more-menu';
 import { MoreWithTriangle } from './more-with-triangle';
@@ -12,8 +14,11 @@ import { TimedMessage } from './timed-message';
 import { ButtonLink } from './button-link';
 
 export default function PostMoreLink({ post, user, ...props }) {
+  const fixedMenu = useMediaQuery('(max-width: 450px)');
+
   const { pivotRef, menuRef, opened, toggle } = useDropDownKbd({
     closeOn: CLOSE_ON_CLICK_OUTSIDE,
+    fixed: fixedMenu,
   });
   const [serverInfo, serverInfoStatus] = useServerInfo();
 
@@ -41,6 +46,21 @@ export default function PostMoreLink({ post, user, ...props }) {
     label = <TimedMessage message="Error!">More</TimedMessage>;
   }
 
+  useEffect(() => {
+    if (fixedMenu && opened) {
+      // Fix scroll position
+      const menuTop = document.documentElement.clientHeight - menuRef.current.scrollHeight;
+      const linkBottom = pivotRef.current.getBoundingClientRect().bottom;
+      if (linkBottom > menuTop) {
+        intentToScroll();
+        window.scrollBy({
+          top: linkBottom - menuTop + 4,
+          behavior: 'smooth',
+        });
+      }
+    }
+  }, [fixedMenu, opened, menuRef, pivotRef]);
+
   return (
     <>
       <ButtonLink
@@ -67,6 +87,7 @@ export default function PostMoreLink({ post, user, ...props }) {
             doAndClose={doAndClose}
             permalink={canonicalPostURI}
             toggleSave={props.toggleSave}
+            fixed={fixedMenu}
           />
         </Portal>
       )}
