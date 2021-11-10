e8d1693419f8ea5504724ba79a186f94f2a06063
Alexey Zakhlestin <indeyets@gmail.com>
2021-11-10 20:44:13 +0300

    Add native previews for Apple Music

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## Not released
+### Added
+- Native previews for Apple Music
+
 ## [1.102.4] - 2021-10-22
 ### Fixed
 - Fix inverted autoplay setting for Vimeo (introduced in 1.102.3)
--- src/components/link-preview/preview.js
+++ src/components/link-preview/preview.js
@@ -10,6 +10,7 @@ import TelegramPreview, { canShowURL as telegramCanShowURL } from './telegram';
 import TikTokPreview, { canShowURL as tikTokCanShowURL } from './tiktok';
 import SoundCloudPreview, { canShowURL as soundCloudCanShowURL } from './soundcloud';
 import SpotifyPreview, { canShowURL as spotifyCanShowURL } from './spotify';
+import AppleMusicPreview, { canShowUrl as appleMusicCanShowURL } from './apple-music';
 import EmbedlyPreview from './embedly';
 
 export default function LinkPreview({ allowEmbedly, url }) {
@@ -36,6 +37,8 @@ export default function LinkPreview({ allowEmbedly, url }) {
     return <SoundCloudPreview url={url} />;
   } else if (spotifyCanShowURL(url)) {
     return <SpotifyPreview url={url} />;
+  } else if (appleMusicCanShowURL(url)) {
+    return <AppleMusicPreview url={url} />;
   }
 
   if (allowEmbedly) {
