a5ff1da1ed65b3008bfbad107378bc080134fe25
Alexey Zakhlestin <indeyets@gmail.com>
2022-01-05 21:05:51 +0300

    Upgrade dependencies

--- webpack/rules.js
+++ webpack/rules.js
@@ -38,7 +38,7 @@ class RuleGenerator {
                 {
                   modules: false,
                   useBuiltIns: 'entry',
-                  corejs: '3.19',
+                  corejs: '3.20',
                 },
               ],
             ]),
