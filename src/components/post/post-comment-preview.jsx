41d92e778554eac65b4d865c2565dd60902ea488
David Mzareulyan <david@hiero.ru>
2022-04-12 13:10:20 +0300

    Load comment preview if the comment isn't in store

--- src/components/post/post-comment-preview.jsx
+++ src/components/post/post-comment-preview.jsx
@@ -1,7 +1,13 @@
-import { useEffect, useRef } from 'react';
-import { useSelector } from 'react-redux';
+import { useMemo, useEffect, useRef } from 'react';
+
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router';
-import { READMORE_STYLE_COMPACT } from '../../utils/frontend-preferences-options';
+import { getCommentByNumber } from '../../redux/action-creators';
+import { initialAsyncState } from '../../redux/async-helpers';
+import {
+  COMMENT_HIDDEN_BANNED,
+  READMORE_STYLE_COMPACT,
+} from '../../utils/frontend-preferences-options';
 
 import { commentReadmoreConfig } from '../../utils/readmore-config';
 import Expandable from '../expandable';
@@ -11,11 +17,31 @@ import UserName from '../user-name';
 import styles from './post-comment-preview.module.scss';
 
 export function PostCommentPreview({ postId, seqNumber, postUrl, close }) {
+  const dispatch = useDispatch();
+  const allComments = useSelector((state) => state.comments);
+  const allUsers = useSelector((state) => state.users);
+  const getCommentStatuses = useSelector((state) => state.getCommentStatuses);
   const readMoreStyle = useSelector((state) => state.user.frontendPreferences.readMoreStyle);
-  const comment = useSelector((state) =>
-    Object.values(state.comments).find((c) => c.postId === postId && c.seqNumber === seqNumber),
+
+  const comment = useMemo(
+    () => Object.values(allComments).find((c) => c.postId === postId && c.seqNumber === seqNumber),
+    [allComments, postId, seqNumber],
   );
-  const author = useSelector((state) => state.users[comment?.createdBy]);
+  const author = allUsers[comment?.createdBy];
+
+  const commentBody = useMemo(() => {
+    if (comment?.hideType === COMMENT_HIDDEN_BANNED) {
+      return 'Comment from blocked user';
+    }
+    return comment?.body;
+  }, [comment]);
+
+  const getCommentStatus = getCommentStatuses[`${postId}#${seqNumber}`] || initialAsyncState;
+  useEffect(() => {
+    if (!comment && getCommentStatus.initial) {
+      dispatch(getCommentByNumber(postId, seqNumber));
+    }
+  }, [comment, dispatch, getCommentStatus.initial, postId, seqNumber]);
 
   const boxRef = useRef();
   useEffect(() => {
@@ -35,19 +61,27 @@ export function PostCommentPreview({ postId, seqNumber, postUrl, close }) {
 
   return (
     <div ref={boxRef} className={styles['box']}>
-      <Expandable
-        expanded={readMoreStyle === READMORE_STYLE_COMPACT}
-        config={commentReadmoreConfig}
-        bonusInfo={commentTail}
-      >
-        <PieceOfText text={comment?.body} />
-        {commentTail}
-      </Expandable>
-      <div className={styles['actions']}>
-        <Link to={`${postUrl}#comment-${comment.id}`} onClick={close}>
-          Go to comment
-        </Link>
-      </div>
+      {comment ? (
+        <>
+          <Expandable
+            expanded={readMoreStyle === READMORE_STYLE_COMPACT}
+            config={commentReadmoreConfig}
+            bonusInfo={commentTail}
+          >
+            <PieceOfText text={commentBody} />
+            {commentTail}
+          </Expandable>
+          <div className={styles['actions']}>
+            <Link to={`${postUrl}#comment-${comment?.id}`} onClick={close}>
+              Go to comment
+            </Link>
+          </div>
+        </>
+      ) : getCommentStatus.error ? (
+        <div className={styles['error']}>Error: {getCommentStatus.errorText}</div>
+      ) : (
+        <div className={styles['loading']}>Loading...</div>
+      )}
     </div>
   );
 }
