4b847d82dbf5383884db76fdc73a9eb36fc6c4a7
max-ermilov <max-ermilov@yandex.ru>
2022-11-08 14:36:56 +0300

    fix private u invisible after sub approve

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1074,6 +1074,27 @@ export function user(state = initUser(), action) {
         },
       };
     }
+    case ActionTypes.REALTIME_INCOMING_EVENT: {
+      if (action.payload.event === 'event:new') {
+        const { Notifications, users } = action.payload.data;
+        for (const note of Notifications) {
+          if (note.event_type === 'subscription_request_approved') {
+            const user = users.find((u) => u.id === note.created_user_id);
+            if (!(user.id in state.subscriptions)) {
+              return {
+                ...state,
+                subscriptions: [...state.subscriptions, user.id],
+                pendingSubscriptionRequests: _.without(
+                  state.pendingSubscriptionRequests || [],
+                  user.id,
+                ),
+              };
+            }
+          }
+        }
+      }
+      return state;
+    }
     case ActionTypes.REALTIME_GLOBAL_USER_UPDATE: {
       const userId = action.user.id;
       if (state.id === userId) {
