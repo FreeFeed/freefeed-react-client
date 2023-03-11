e26e2103777e9c9b19830e8668e3f5242d424dba
David Mzareulyan <david@hiero.ru>
2023-03-11 11:12:23 +0300

    Release 1.116.1

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -7,7 +7,7 @@ export default function Footer({ short }) {
   return (
     <footer className="footer">
       <p role="navigation">
-        &copy; FreeFeed 1.116.0 (Mar 10, 2023)
+        &copy; FreeFeed 1.116.1 (Mar 11, 2023)
         <br />
         <Link to="/about">About</Link>
         {' | '}
