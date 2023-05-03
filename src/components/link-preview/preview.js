61bc0bf8f84ff22eb215e7dc280234121e538eb9
David Mzareulyan <david@hiero.ru>
2023-05-03 14:46:57 +0300

    Add support for Aparat.com video hosting

--- src/components/link-preview/preview.js
+++ src/components/link-preview/preview.js
@@ -11,7 +11,6 @@ import TikTokPreview, { canShowURL as tikTokCanShowURL } from './tiktok';
 import SoundCloudPreview, { canShowURL as soundCloudCanShowURL } from './soundcloud';
 import SpotifyPreview, { canShowURL as spotifyCanShowURL } from './spotify';
 import AppleMusicPreview, { canShowUrl as appleMusicCanShowURL } from './apple-music';
-import AparatPreview, { canShowURL as aparatCanShowURL } from './aparat';
 
 import EmbedlyPreview from './embedly';
 
@@ -41,8 +40,6 @@ export default function LinkPreview({ allowEmbedly, url }) {
     return <SpotifyPreview url={url} />;
   } else if (appleMusicCanShowURL(url)) {
     return <AppleMusicPreview url={url} />;
-  } else if (aparatCanShowURL(url)) {
-    return <AparatPreview url={url} />;
   }
 
   if (allowEmbedly) {
