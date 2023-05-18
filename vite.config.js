8504539779656d16508055a4b528c87bfb3a00ee
David Mzareulyan <david@hiero.ru>
2023-05-18 11:14:40 +0300

    Expose process.* globals

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   build: { outDir: '_dist' },
+  define: {
+    'process.platform': JSON.stringify('web'),
+    'process.env.TZ': JSON.stringify('UTC'),
+    'process.env.NODE_ENV': JSON.stringify(
+      process.env.NODE_ENV === 'production' ? 'production' : 'development',
+    ),
+  },
 });
