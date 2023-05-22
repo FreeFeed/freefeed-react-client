4551b5fe0128b13593ac0bef22487388915f49ba
David Mzareulyan <david@hiero.ru>
2023-05-22 18:40:59 +0300

    Add rel="preload" links for early preload main app script and style

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import VitePluginInjectPreload from 'vite-plugin-inject-preload';
 
 export default defineConfig(({ mode }) => ({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginInjectPreload({
+      files: [
+        { match: /\/app-\w+\.js$/, attributes: { rel: 'modulepreload' } },
+        { match: /\/app-\w+\.css$/ },
+      ],
+    }),
+  ],
   build: { outDir: '_dist' },
   define:
     mode !== 'test'
