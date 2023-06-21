5e04918b946b100a81606026555d2be4b5156c71
David Mzareulyan <david@hiero.ru>
2023-06-21 13:37:03 +0300

    Pass ref to lazyComponent

--- src/components/lazy-component.jsx
+++ src/components/lazy-component.jsx
@@ -1,21 +1,20 @@
-import { Suspense, useState, useEffect, useMemo } from 'react';
+import { Suspense, useState, useEffect, useMemo, forwardRef } from 'react';
 import { lazyRetry } from '../utils/retry-promise';
 import ErrorBoundary from './error-boundary';
 
-export const lazyComponent =
-  (loader, { fallback, errorMessage, delay }) =>
-  (props) => {
+export const lazyComponent = (loader, { fallback, errorMessage, delay }) =>
+  forwardRef((props, ref) => {
     // No deps: loader can depends only on the initial props
     // eslint-disable-next-line react-hooks/exhaustive-deps
     const Lazy = useMemo(() => lazyRetry(() => loader(props)), []);
     return (
       <ErrorBoundary message={errorMessage}>
         <Suspense fallback={<Delayed delay={delay}>{fallback}</Delayed>}>
-          <Lazy {...props} />
+          <Lazy {...props} ref={ref} />
         </Suspense>
       </ErrorBoundary>
     );
-  };
+  });
 
 export function Delayed({ delay = 500, children }) {
   const withDelay = children && delay > 0;
