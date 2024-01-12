eb057fe183ed7ae092a03fd6ce552be65a203346
David Mzareulyan <david@hiero.ru>
2024-01-12 23:20:01 +0300

    Add a "toggle fullscreen" button to the lightbox

--- src/services/lightbox.js
+++ src/services/lightbox.js
@@ -4,6 +4,11 @@ import PhotoSwipeLightbox from 'photoswipe/lightbox';
 import Mousetrap from 'mousetrap';
 import 'photoswipe/photoswipe.css';
 import '../../styles/shared/lighbox.scss';
+import { renderToString } from 'react-dom/server';
+import { createElement } from 'react';
+import { faCompress, faExpand } from '@fortawesome/free-solid-svg-icons';
+import { getFullscreenAPI } from '../utils/fullscreen';
+import { Icon } from '../components/fontawesome-icons';
 
 const prevHotKeys = ['a', 'ф', 'h', 'р', '4'];
 const nextHotKeys = ['d', 'в', 'k', 'л', '6'];
@@ -12,10 +17,45 @@ export function openLightbox(index, dataSource) {
   initLightbox().loadAndOpen(index, dataSource);
 }
 
+const fsApi = getFullscreenAPI();
+
+const expandIconHTML = renderToString(createElement(Icon, { icon: faExpand }));
+const compressIconHTML = renderToString(createElement(Icon, { icon: faCompress }));
+
 function initLightbox() {
   const lightbox = new PhotoSwipeLightbox({
     clickToCloseNonZoomable: false,
     pswpModule: () => import('photoswipe'),
+  });
+
+  // Add fullscreen button
+  lightbox.on('uiRegister', () => {
+    if (!fsApi) {
+      return;
+    }
+    lightbox.pswp.ui.registerElement({
+      name: 'fs',
+      ariaLabel: 'Full screen',
+      order: 9,
+      isButton: true,
+      html: expandIconHTML,
+      onClick: () => {
+        if (fsApi.isFullscreen()) {
+          fsApi.exit();
+        } else {
+          fsApi.request(lightbox.pswp.element);
+        }
+      },
+    });
+
+    const h = () => {
+      const btn = document.querySelector('.pswp__button--fs');
+      btn.innerHTML = fsApi.isFullscreen() ? compressIconHTML : expandIconHTML;
+    };
+
+    document.addEventListener(fsApi.changeEvent, h);
+    lightbox.on('destroy', () => document.removeEventListener(fsApi.changeEvent, h));
+    lightbox.on('close', () => fsApi.isFullscreen() && fsApi.exit());
   });
 
   // Add filters for the correct open/close animation
