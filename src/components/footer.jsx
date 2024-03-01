6f6791b81c4b701162e817389f9ad03771d2a5ee
David Mzareulyan <david@hiero.ru>
2024-03-01 17:10:39 +0300

    Release 1.128.0

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -7,7 +7,7 @@ export default function Footer({ short }) {
   return (
     <footer className="footer">
       <p role="navigation">
-        &copy; FreeFeed 1.128.0 (Not released)
+        &copy; FreeFeed 1.128.0 (Mar 1, 2024)
         <br />
         <Link to="/about">About</Link>
         {' | '}
