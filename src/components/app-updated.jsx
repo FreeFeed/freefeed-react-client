9d25d0eb7abb4e2e6b00f00c255aab3e5620f778
David Mzareulyan <david@hiero.ru>
2024-08-24 12:49:02 +0300

    Use 'vite-plugin-pwa' to generate the service worker and PWA manifest
    The service worker caches all site assets, including web fonts. It also caches user profile pictures (up to 100 entries). The AppUpdated component periodically checks the service worker for update.

--- src/components/app-updated.jsx
+++ src/components/app-updated.jsx
@@ -1,17 +1,37 @@
 /* global CONFIG */
 import { useSelector } from 'react-redux';
+import { useRegisterSW } from 'virtual:pwa-register/react';
 
+import { useEvent } from 'react-use-event-hook';
 import styles from './app-updated.module.scss';
 import { ButtonLink } from './button-link';
 
-function reloadPage() {
-  window.location.reload(true);
-}
+const { intervalSec } = CONFIG.appVersionCheck;
 
 export function AppUpdated() {
   const updated = useSelector((state) => state.appUpdated.updated);
 
-  if (!updated) {
+  const {
+    needRefresh: [needRefresh],
+    updateServiceWorker,
+  } = useRegisterSW({
+    onRegistered(r) {
+      r && setInterval(() => r.update(), intervalSec * 1000);
+    },
+  });
+
+  const reloadPage = useEvent(() => {
+    if (needRefresh) {
+      updateServiceWorker();
+      // Sometimes the updateServiceWorker doesn't refresh the page, so reload
+      // it manually after some time
+      setTimeout(() => window.location.reload(true), 2000);
+    } else {
+      window.location.reload(true);
+    }
+  });
+
+  if (!updated && !needRefresh) {
     return null;
   }
 
