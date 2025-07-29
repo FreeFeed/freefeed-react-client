f83f83b72d7ee0550b94f4cf4f2f30d69409c6e3
David Mzareulyan <david@hiero.ru>
2025-07-29 10:53:33 +0300

    Use version file check for all Safari browsers

--- src/components/app-updated.jsx
+++ src/components/app-updated.jsx
@@ -4,11 +4,15 @@ import { useRegisterSW } from 'virtual:pwa-register/react';
 
 import { useEvent } from 'react-use-event-hook';
 import { useState } from 'react';
-import { isIos } from '../utils/platform-detection';
+import { isSafari } from '../utils/platform-detection';
 import styles from './app-updated.module.scss';
 import { ButtonLink } from './button-link';
 
 const { intervalSec } = CONFIG.appVersionCheck;
+
+// Safari has strange behavior with service workers updates, so we will use
+// the old-fashion way (via the version file) here
+const useServiceWorker = !isSafari;
 
 export function AppUpdated() {
   const versionFileState = useSelector((state) => state.appUpdated);
@@ -25,7 +29,7 @@ export function AppUpdated() {
     onRegistered(r) {
       // iOS has strange behavior with service workers updates, so we will use
       // the old-fashion way (via the version file) here
-      if (r && !isIos) {
+      if (useServiceWorker && r) {
         setSwRegistered(true);
         setInterval(() => r.update(), intervalSec * 1000);
       }
@@ -33,7 +37,7 @@ export function AppUpdated() {
   });
 
   const reloadPage = useEvent(() => {
-    if (workerUpdated && !isIos) {
+    if (useServiceWorker && workerUpdated) {
       updateServiceWorker();
       // Sometimes the updateServiceWorker doesn't refresh the page, so reload
       // it manually after some time
